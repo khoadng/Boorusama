3f326849edfc18d08b8ec9ea6a4e25778033d3b9
khoaharp <nguyenduckhoa1996@gmail.com>
2020-12-13 00:00:55 +0700

    make bloc observer easier to read

--- lib/bloc_observer.dart
+++ lib/bloc_observer.dart
@@ -3,19 +3,34 @@ import 'package:bloc/bloc.dart';
 class SimpleBlocObserver extends BlocObserver {
   @override
   void onEvent(Bloc bloc, Object event) {
-    print('onEvent $event');
+    final currentEvent = event.toString();
+    final index = currentEvent.indexOf('(');
+    if (index > 0) {
+      print('EVENT: ${currentEvent.substring(0, index)}');
+    } else {
+      print('EVENT: $event');
+    }
     super.onEvent(bloc, event);
   }
 
   @override
   onTransition(Bloc bloc, Transition transition) {
-    print('onTransition $transition');
+    final currentState = transition.currentState.toString();
+    final nextState = transition.nextState.toString();
+    final indexCurrent = currentState.indexOf('(');
+    final indexNext = nextState.indexOf('(');
+    if (indexNext > 0 && indexCurrent > 0) {
+      print(
+          'TRANSITION: [${currentState.substring(0, indexCurrent)}] => [${nextState.substring(0, indexNext)}]');
+    } else {
+      print('TRANSITION: [$currentState] => [$nextState]');
+    }
     super.onTransition(bloc, transition);
   }
 
   @override
   void onError(Cubit cubit, Object error, StackTrace stackTrace) {
-    print('onError $error');
+    print('ERROR: $error');
     super.onError(cubit, error, stackTrace);
   }
 }
