f3b6a98a851e1a26f379059c12ada62148a645b8
khoaharp <nguyenduckhoa1996@gmail.com>
2021-01-18 09:06:49 +0700

    update precacheimage

--- lib/presentation/features/home/post_image.dart
+++ lib/presentation/features/home/post_image.dart
@@ -2,7 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:time/time.dart';
 
-class PostImage extends StatelessWidget {
+class PostImage extends StatefulWidget {
   const PostImage({
     Key key,
     @required this.imageUrl,
@@ -11,15 +11,38 @@ class PostImage extends StatelessWidget {
   final String imageUrl;
 
   @override
+  _PostImageState createState() => _PostImageState();
+}
+
+class _PostImageState extends State<PostImage> {
+  Image myImage;
+
+  @override
+  void initState() {
+    myImage = Image(
+      image: CachedNetworkImageProvider(widget.imageUrl),
+    );
+    myImage.image
+        .resolve(ImageConfiguration())
+        .addListener(ImageStreamListener((_, __) {}));
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    precacheImage(myImage.image, context);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return CachedNetworkImage(
-      imageUrl: imageUrl,
+      imageUrl: widget.imageUrl,
       imageBuilder: (context, imageProvider) {
-        precacheImage(imageProvider, context);
         return Container(
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(8.0),
-            image: DecorationImage(image: imageProvider, fit: BoxFit.cover),
+            image: DecorationImage(image: myImage.image, fit: BoxFit.cover),
           ),
         );
       },
