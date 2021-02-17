61ddc0f2256b1ffa0295c1f363b17d0240538cd6
khoaharp <nguyenduckhoa1996@gmail.com>
2021-02-17 14:23:38 +0700

    remove more boilerplates

--- lib/boorus/danbooru/presentation/features/wiki/wiki_page.dart
+++ lib/boorus/danbooru/presentation/features/wiki/wiki_page.dart
@@ -1,16 +1,26 @@
 // Flutter imports:
+import 'package:boorusama/boorus/danbooru/domain/wikis/wiki.dart';
+import 'package:boorusama/boorus/danbooru/infrastructure/repositories/wikis/wiki_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 // Package imports:
 import 'package:flutter_riverpod/all.dart';
 
 // Project imports:
-import 'package:boorusama/boorus/danbooru/application/wiki/wiki_state_notifier.dart';
+import 'package:hooks_riverpod/all.dart';
 
-final wikiStateNotifierProvider =
-    StateNotifierProvider<WikiStateNotifier>((ref) => WikiStateNotifier(ref));
+final _wikiProvider =
+    FutureProvider.autoDispose.family<Wiki, String>((ref, subject) async {
+  final repo = ref.watch(wikiProvider);
+  final wiki = await repo.getWikiFor(subject);
 
-class WikiPage extends StatefulWidget {
+  ref.maintainState = true;
+
+  return wiki;
+});
+
+class WikiPage extends HookWidget {
   final String title;
 
   const WikiPage({
@@ -19,39 +29,22 @@ class WikiPage extends StatefulWidget {
   }) : super(key: key);
 
   @override
-  _WikiPageState createState() => _WikiPageState();
-}
-
-class _WikiPageState extends State<WikiPage> {
-  @override
-  void initState() {
-    super.initState();
-    Future.delayed(Duration.zero,
-        () => context.read(wikiStateNotifierProvider).getWiki(widget.title));
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final wiki = useProvider(_wikiProvider(title));
+
     return Material(
       child: SafeArea(
         child: Scaffold(
           appBar: AppBar(
-            title: Center(child: Text(widget.title)),
+            title: Center(child: Text(title)),
           ),
-          body: Consumer(
-            builder: (context, watch, child) {
-              final state = watch(wikiStateNotifierProvider.state);
-              return state.when(
-                initial: () => Center(child: CircularProgressIndicator()),
-                loading: () => Center(child: CircularProgressIndicator()),
-                fetched: (wiki) =>
-                    SingleChildScrollView(child: Text(wiki.body)),
-                error: (name, message) => Center(
-                  child: Text(message),
-                ),
-              );
-            },
-          ),
+          body: wiki.when(
+              data: (wiki) => SingleChildScrollView(child: Text(wiki.body)),
+              loading: () => Center(
+                    child: CircularProgressIndicator(),
+                  ),
+              error: (obj, stackTrace) =>
+                  Center(child: Text("Something went wrong"))),
         ),
       ),
     );
