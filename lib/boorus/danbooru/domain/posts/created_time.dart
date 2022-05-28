32e1ae443fc4cfd438d277175bebc50f6051fa1f
khoaharp <nguyenduckhoa1996@gmail.com>
2022-05-28 15:53:13 +0700

    fix: fix datetime display

--- lib/boorus/danbooru/domain/posts/created_time.dart
+++ lib/boorus/danbooru/domain/posts/created_time.dart
@@ -1,3 +1,5 @@
+import 'package:timeago/timeago.dart' as timeago;
+
 class CreatedTime {
   final String _createdDate;
 
@@ -9,24 +11,8 @@ class CreatedTime {
   String toString() {
     final now = DateTime.now();
     final diff = now.difference(time);
+    final ago = now.subtract(diff);
 
-    if (diff.inSeconds < 60) {
-      return "${diff.inSeconds} second${diff.inSeconds >= 2 ? 's' : ''} ago";
-    } else if (diff.inMinutes < 60) {
-      return "${diff.inMinutes} minute${diff.inMinutes >= 2 ? 's' : ''} ago";
-    } else if (diff.inHours < 24) {
-      return "${diff.inHours} hour${diff.inHours >= 2 ? 's' : ''} ago";
-      //TODO: handle other case of datetime here
-    } else {
-      if (diff.inDays < 31) {
-        return "${diff.inDays} day${diff.inDays >= 2 ? 's' : ''} ago";
-      } else if (diff.inDays < 365) {
-        var month = diff.inDays ~/ 12;
-        return "$month month${month >= 2 ? 's' : ''} ago";
-      } else {
-        var year = diff.inDays ~/ 365;
-        return "$year year${year >= 2 ? 's' : ''} ago";
-      }
-    }
+    return timeago.format(ago);
   }
 }
