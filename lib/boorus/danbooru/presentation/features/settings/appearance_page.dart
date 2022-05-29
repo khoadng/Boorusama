9ba0e2dbe432050a856177d6d90f0fe428caa5e6
khoaharp <nguyenduckhoa1996@gmail.com>
2022-05-29 13:59:30 +0700

    chore: remove theme

--- lib/boorus/danbooru/presentation/features/settings/appearance_page.dart
+++ lib/boorus/danbooru/presentation/features/settings/appearance_page.dart
@@ -6,7 +6,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 // Project imports:
 import 'package:boorusama/boorus/danbooru/application/settings/settings.dart';
-import 'package:boorusama/boorus/danbooru/application/themes/theme_state_notifier.dart';
 import 'package:easy_localization/easy_localization.dart';
 
 class AppearancePage extends StatefulWidget {
@@ -31,29 +30,22 @@ class _AppearancePageState extends State<AppearancePage> {
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
-            Text('settings.appSettings.appearance.theme._string'.tr()),
-            RadioListTile<ThemeMode>(
-              title: Text('settings.appSettings.appearance.theme.dark'.tr()),
-              value: ThemeMode.dark,
-              groupValue: widget.settings.themeMode,
-              onChanged: (value) => setTheme(value, context),
-            ),
-            RadioListTile<ThemeMode>(
-              title: Text('settings.appSettings.appearance.theme.light'.tr()),
-              value: ThemeMode.light,
-              groupValue: widget.settings.themeMode,
-              onChanged: (value) => setTheme(value, context),
-            ),
+            // Text('settings.appSettings.appearance.theme._string'.tr()),
+            // RadioListTile<ThemeMode>(
+            //   title: Text('settings.appSettings.appearance.theme.dark'.tr()),
+            //   value: ThemeMode.dark,
+            //   groupValue: widget.settings.themeMode,
+            //   onChanged: (value) => setTheme(value, context),
+            // ),
+            // RadioListTile<ThemeMode>(
+            //   title: Text('settings.appSettings.appearance.theme.light'.tr()),
+            //   value: ThemeMode.light,
+            //   groupValue: widget.settings.themeMode,
+            //   onChanged: (value) => setTheme(value, context),
+            // ),
           ],
         ),
       ),
     );
-  }
-
-  void setTheme(ThemeMode value, BuildContext context) {
-    setState(() {
-      widget.settings.themeMode = value;
-    });
-    context.read(themeStateNotifierProvider).changeTheme(value);
   }
 }
