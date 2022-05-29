625b8e068dab0fa50e5892b8c42d94c0968f58e3
khoaharp <nguyenduckhoa1996@gmail.com>
2022-05-29 17:13:08 +0700

    fix: move downloaded files to download folder

--- lib/core/infrastructure/IOHelper.dart
+++ lib/core/infrastructure/IOHelper.dart
@@ -12,28 +12,32 @@ class IOHelper {
     try {
       return await _getLocalPath(folderName);
     } catch (e) {
-      final directory =
-          Platform.isAndroid ? await getExternalStorageDirectory() : await getApplicationDocumentsDirectory();
+      final directory = Platform.isAndroid
+          ? await getExternalStorageDirectory()
+          : await getApplicationDocumentsDirectory();
       return directory.path + Platform.pathSeparator + folderName;
     }
   }
 
   static Future<String> getLocalPathFallback() async {
-    final directory =
-        Platform.isAndroid ? await getExternalStorageDirectory() : await getApplicationDocumentsDirectory();
+    final directory = Platform.isAndroid
+        ? await getExternalStorageDirectory()
+        : await getApplicationDocumentsDirectory();
     return directory.path;
   }
 
   static Future<String> _getLocalPath(String folderName) async {
-    Future<String> createDir(Directory root) async => root.path + Platform.pathSeparator + folderName;
+    Future<String> createDir(Directory root) async =>
+        root.path + Platform.pathSeparator + folderName;
 
     String path = '${Platform.pathSeparator}$folderName';
     if (Platform.isAndroid) {
-      final root = Directory.fromUri(Uri(path: '/storage/emulated/0/'));
+      final root = Directory.fromUri(Uri(path: '/storage/emulated/0/Download'));
 
       path = await createDir(root);
     } else if (Platform.isWindows) {
-      final root = (await getDownloadsDirectory()) ?? (await getApplicationDocumentsDirectory());
+      final root = (await getDownloadsDirectory()) ??
+          (await getApplicationDocumentsDirectory());
       path = await createDir(root);
     } else {
       throw UnimplementedError();
