b8e775666700d508eddd636e3c67b7987aaf6c81
khoaharp <nguyenduckhoa1996@gmail.com>
2022-05-29 14:43:32 +0700

    chore: remove freezed in artist commentary

--- lib/boorus/danbooru/domain/posts/artist_commentary_dto.dart
+++ lib/boorus/danbooru/domain/posts/artist_commentary_dto.dart
@@ -1,35 +1,46 @@
-// Package imports:
-import 'package:freezed_annotation/freezed_annotation.dart';
-
 // Project imports:
 import 'package:boorusama/boorus/danbooru/domain/posts/artist_commentary.dart';
 
-part 'artist_commentary_dto.freezed.dart';
-part 'artist_commentary_dto.g.dart';
+class ArtistCommentaryDto {
+  ArtistCommentaryDto({
+    this.id,
+    this.postId,
+    this.originalTitle,
+    this.originalDescription,
+    this.translatedTitle,
+    this.translatedDescription,
+    this.createdAt,
+    this.updatedAt,
+  });
 
-@freezed
-abstract class ArtistCommentaryDto with _$ArtistCommentaryDto {
-  const factory ArtistCommentaryDto({
-    @nullable int id,
-    @nullable int post_id,
-    @nullable String original_title,
-    @nullable String original_description,
-    @nullable String translated_title,
-    @nullable String translated_description,
-    @nullable String created_at,
-    @nullable String updated_at,
-  }) = _ArtistCommentaryDto;
+  final int id;
+  final int postId;
+  final String originalTitle;
+  final String originalDescription;
+  final String translatedTitle;
+  final String translatedDescription;
+  final DateTime createdAt;
+  final DateTime updatedAt;
 
   factory ArtistCommentaryDto.fromJson(Map<String, dynamic> json) =>
-      _$ArtistCommentaryDtoFromJson(json);
+      ArtistCommentaryDto(
+        id: json["id"],
+        postId: json["post_id"],
+        originalTitle: json["original_title"],
+        originalDescription: json["original_description"],
+        translatedTitle: json["translated_title"],
+        translatedDescription: json["translated_description"],
+        createdAt: DateTime.parse(json["created_at"]),
+        updatedAt: DateTime.parse(json["updated_at"]),
+      );
 }
 
 extension ArtistCommentaryDtoX on ArtistCommentaryDto {
   ArtistCommentary toEntity() {
     return ArtistCommentary(
-        originalTitle: original_title ?? "",
-        originalDescription: original_description ?? "",
-        translatedTitle: translated_title ?? "",
-        translatedDescription: translated_description ?? "");
+        originalTitle: originalTitle ?? "",
+        originalDescription: originalDescription ?? "",
+        translatedTitle: translatedTitle ?? "",
+        translatedDescription: translatedDescription ?? "");
   }
 }
