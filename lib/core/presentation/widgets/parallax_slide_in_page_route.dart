a0c8dd0d065281fc338d627ce8cca9a03a64808f
khoa.nguyen <khoa.nguyen@enish.com>
2022-06-22 14:34:08 +0700

    chore: create a general func for parallax animation

--- lib/core/presentation/widgets/parallax_slide_in_page_route.dart
+++ lib/core/presentation/widgets/parallax_slide_in_page_route.dart
@@ -1,6 +1,33 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
 
+RouteTransitionsBuilder parallaxSlideInTransitionBuilder(
+  Widget enterWidget,
+  Widget oldWidget,
+) =>
+    (context, animation, secondaryAnimation, child) => Stack(
+          children: <Widget>[
+            SlideTransition(
+                position: Tween<Offset>(
+                  begin: Offset.zero,
+                  end: const Offset(-1, 0),
+                ).animate(
+                  CurvedAnimation(
+                      parent: animation, curve: Curves.fastOutSlowIn),
+                ),
+                child: oldWidget),
+            SlideTransition(
+                position: Tween<Offset>(
+                  begin: const Offset(1, 0),
+                  end: Offset.zero,
+                ).animate(
+                  CurvedAnimation(
+                      parent: animation, curve: Curves.fastOutSlowIn),
+                ),
+                child: enterWidget),
+          ],
+        );
+
 class ParallaxSlideInPageRoute extends PageRouteBuilder {
   ParallaxSlideInPageRoute({
     required this.enterWidget,
@@ -10,29 +37,8 @@ class ParallaxSlideInPageRoute extends PageRouteBuilder {
           transitionDuration: const Duration(milliseconds: 350),
           settings: settings,
           pageBuilder: (context, animation, secondaryAnimation) => enterWidget,
-          transitionsBuilder: (context, animation, secondaryAnimation, child) =>
-              Stack(
-            children: <Widget>[
-              SlideTransition(
-                  position: Tween<Offset>(
-                    begin: Offset.zero,
-                    end: const Offset(-1, 0),
-                  ).animate(
-                    CurvedAnimation(
-                        parent: animation, curve: Curves.fastOutSlowIn),
-                  ),
-                  child: oldWidget),
-              SlideTransition(
-                  position: Tween<Offset>(
-                    begin: const Offset(1, 0),
-                    end: Offset.zero,
-                  ).animate(
-                    CurvedAnimation(
-                        parent: animation, curve: Curves.fastOutSlowIn),
-                  ),
-                  child: enterWidget),
-            ],
-          ),
+          transitionsBuilder:
+              parallaxSlideInTransitionBuilder(enterWidget, oldWidget),
         );
 
   final Widget enterWidget;
