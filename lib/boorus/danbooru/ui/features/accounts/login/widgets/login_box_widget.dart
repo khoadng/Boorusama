b717ed29d49448a0a63762288329896c8b817e77
khoaharp <nguyenduckhoa1996@gmail.com>
2022-09-23 17:45:58 +0700

    feat: add a login notice in login page

--- lib/boorus/danbooru/ui/features/accounts/login/widgets/login_box_widget.dart
+++ lib/boorus/danbooru/ui/features/accounts/login/widgets/login_box_widget.dart
@@ -103,7 +103,9 @@ class LoginBox extends HookWidget {
                               child: IconButton(
                                   splashColor: Colors.transparent,
                                   icon: const FaIcon(
-                                      FontAwesomeIcons.solidCircleXmark),
+                                    FontAwesomeIcons.solidCircleXmark,
+                                    size: 18,
+                                  ),
                                   onPressed: usernameTextController.clear),
                             )
                           : null,
@@ -126,8 +128,14 @@ class LoginBox extends HookWidget {
                       suffixIcon: IconButton(
                           splashColor: Colors.transparent,
                           icon: showPassword.value
-                              ? const FaIcon(FontAwesomeIcons.solidEyeSlash)
-                              : const FaIcon(FontAwesomeIcons.solidEye),
+                              ? const FaIcon(
+                                  FontAwesomeIcons.solidEyeSlash,
+                                  size: 18,
+                                )
+                              : const FaIcon(
+                                  FontAwesomeIcons.solidEye,
+                                  size: 18,
+                                ),
                           onPressed: () =>
                               showPassword.value = !showPassword.value),
                     ),
