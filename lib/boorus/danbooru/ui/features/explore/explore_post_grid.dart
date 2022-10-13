39ccad8452e57ab2ea418ce4844e75538a079482
khoaharp <nguyenduckhoa1996@gmail.com>
2022-10-13 17:02:59 +0700

    refactor(post): extract to a common method for navigating to detail page

--- lib/boorus/danbooru/ui/features/explore/explore_post_grid.dart
+++ lib/boorus/danbooru/ui/features/explore/explore_post_grid.dart
@@ -89,17 +89,14 @@ class ExplorePostGrid extends StatelessWidget {
       return SliverPostGrid(
         posts: posts,
         scrollController: controller,
-        onTap: (post, index) => AppRouter.router.navigateTo(
-          context,
-          '/post/detail',
-          routeSettings: RouteSettings(
-            arguments: [
-              posts,
-              index,
-              controller,
-            ],
-          ),
-        ),
+        onTap: (post, index) {
+          goToDetailPage(
+            context: context,
+            posts: posts,
+            initialIndex: index,
+            postBloc: context.read<PostBloc>(),
+          );
+        },
         onFavoriteUpdated: (postId, value) => context
             .read<PostBloc>()
             .add(PostFavoriteUpdated(postId: postId, favorite: value)),
@@ -111,17 +108,14 @@ class ExplorePostGrid extends StatelessWidget {
         return SliverPostGrid(
           posts: posts,
           scrollController: controller,
-          onTap: (post, index) => AppRouter.router.navigateTo(
-            context,
-            '/post/detail',
-            routeSettings: RouteSettings(
-              arguments: [
-                posts,
-                index,
-                controller,
-              ],
-            ),
-          ),
+          onTap: (post, index) {
+            goToDetailPage(
+              context: context,
+              posts: posts,
+              initialIndex: index,
+              postBloc: context.read<PostBloc>(),
+            );
+          },
           onFavoriteUpdated: (postId, value) => context
               .read<PostBloc>()
               .add(PostFavoriteUpdated(postId: postId, favorite: value)),
