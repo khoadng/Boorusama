f18816d8c382cb7ab66ae204fc786e165afdb414
khoa.nguyen <nguyenduckhoa1996@gmail.com>
2022-10-14 16:15:03 +0700

    feat: add UT for rating

---
+++ test/post/rating_test.dart
@@ -0,0 +1,22 @@
+// Package imports:
+import 'package:test/test.dart';
+
+// Project imports:
+import 'package:boorusama/boorus/danbooru/domain/posts/posts.dart';
+
+void main() {
+  group('[post rating test]', () {
+    test('general',
+        () => expect(mapStringToRating('g'), equals(Rating.general)));
+    test('sensitive',
+        () => expect(mapStringToRating('s'), equals(Rating.sensitive)));
+    test('questionable',
+        () => expect(mapStringToRating('q'), equals(Rating.questionable)));
+    test('explicit',
+        () => expect(mapStringToRating('e'), equals(Rating.explicit)));
+    test(
+        'anything',
+        () =>
+            expect(mapStringToRating('anything'), equals(Rating.questionable)));
+  });
+}
