20ba65644373314aa4aaf0f7cba496ddd2f10d4f
khoa.nguyen <nguyenduckhoa1996@gmail.com>
2022-10-18 20:06:42 +0700

    feat: add test for explore detail page

---
+++ test/explore/datetime_test.dart
@@ -0,0 +1,34 @@
+// Package imports:
+import 'package:test/test.dart';
+
+// Project imports:
+import 'package:boorusama/boorus/danbooru/application/explore/explore_detail_bloc.dart';
+import 'package:boorusama/boorus/danbooru/domain/posts/time_scale.dart';
+
+void main() {
+  group('[explore time test]', () {
+    test('add, day', () {
+      expect(DateTime(1).addTimeScale(TimeScale.day), DateTime(1, 1, 2));
+    });
+
+    test('add, week', () {
+      expect(DateTime(1).addTimeScale(TimeScale.week), DateTime(1, 1, 8));
+    });
+
+    test('add, month', () {
+      expect(DateTime(1).addTimeScale(TimeScale.month), DateTime(1, 2));
+    });
+
+    test('subtract, day', () {
+      expect(DateTime(1, 1, 2).subtractTimeScale(TimeScale.day), DateTime(1));
+    });
+
+    test('subtract, week', () {
+      expect(DateTime(1, 1, 8).subtractTimeScale(TimeScale.week), DateTime(1));
+    });
+
+    test('subtract, month', () {
+      expect(DateTime(1, 2).subtractTimeScale(TimeScale.month), DateTime(1));
+    });
+  });
+}
