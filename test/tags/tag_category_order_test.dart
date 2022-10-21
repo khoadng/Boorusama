bb85856573912f8a8be864c3cc73f674527f44df
khoa.nguyen <nguyenduckhoa1996@gmail.com>
2022-10-21 17:02:50 +0700

    feat: add tag order tests

---
+++ test/tags/tag_category_order_test.dart
@@ -0,0 +1,23 @@
+// Package imports:
+import 'package:test/test.dart';
+
+// Project imports:
+import 'package:boorusama/boorus/danbooru/application/tag/tag.dart';
+import 'package:boorusama/boorus/danbooru/domain/tags/tags.dart';
+
+void main() {
+  group('[tag category order test]', () {
+    test('artist', () => expect(tagCategoryToOrder(TagCategory.artist), 0));
+    test(
+      'copyright',
+      () => expect(tagCategoryToOrder(TagCategory.copyright), 1),
+    );
+    test(
+      'character',
+      () => expect(tagCategoryToOrder(TagCategory.charater), 2),
+    );
+    test('general', () => expect(tagCategoryToOrder(TagCategory.general), 3));
+    test('meta', () => expect(tagCategoryToOrder(TagCategory.meta), 4));
+    test('_invalid', () => expect(tagCategoryToOrder(TagCategory.invalid_), 5));
+  });
+}
