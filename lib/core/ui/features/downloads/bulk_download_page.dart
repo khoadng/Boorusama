6ac5775716e4274e6805dd86c73a9b5d6c7beb52
khoaharp <nguyenduckhoa1996@gmail.com>
2022-10-23 09:58:48 +0700

    add tag selection

--- lib/core/ui/features/downloads/bulk_download_page.dart
+++ lib/core/ui/features/downloads/bulk_download_page.dart
@@ -1,25 +1,70 @@
+import 'package:boorusama/boorus/danbooru/domain/autocompletes/autocomplete.dart';
 import 'package:boorusama/boorus/danbooru/ui/features/post_detail/simple_tag_search_view.dart';
+import 'package:boorusama/boorus/danbooru/ui/shared/shared.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
-class BulkDownloadPage extends StatelessWidget {
+class BulkDownloadPage extends StatefulWidget {
   const BulkDownloadPage({
     super.key,
   });
 
   @override
+  State<BulkDownloadPage> createState() => _BulkDownloadPageState();
+}
+
+class _BulkDownloadPageState extends State<BulkDownloadPage> {
+  final selectedTag = ValueNotifier<AutocompleteData?>(null);
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   elevation: 0,
-      //   backgroundColor: Colors.transparent,
-      //   shadowColor: Colors.transparent,
-      //   automaticallyImplyLeading: false,
-      //   title: SearchBar(enabled: false,),
-      // ),
-      body: SafeArea(
-        child: SimpleTagSearchView(
-          onSelected: (data) => print(data),
-        ),
+      appBar: AppBar(
+        title: const Text('Bulk downloads'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: CustomScrollView(
+              slivers: [
+                SliverToBoxAdapter(
+                  child: Center(
+                    child: ValueListenableBuilder<AutocompleteData?>(
+                      valueListenable: selectedTag,
+                      builder: (context, value, child) {
+                        return value == null
+                            ? Container(
+                                width: MediaQuery.of(context).size.width,
+                                height: 100,
+                                color: Theme.of(context).cardColor,
+                                child: const Text('Empty'),
+                              )
+                            : Text('${value.label} (${value.postCount}) ');
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 8),
+            child: SearchBar(
+              enabled: false,
+              hintText: 'Add tag',
+              onTap: () {
+                showBarModalBottomSheet(
+                  context: context,
+                  builder: (context) => SimpleTagSearchView(
+                    onSelected: (tag) {
+                      selectedTag.value = tag;
+                    },
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
