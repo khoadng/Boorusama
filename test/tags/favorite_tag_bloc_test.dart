56c00145da6c02cd0f4aeaedc239fc71554a3dd9
khoa.nguyen <nguyenduckhoa1996@gmail.com>
2022-11-15 07:25:33 +0700

    chore: add unit tests

---
+++ test/tags/favorite_tag_bloc_test.dart
@@ -0,0 +1,115 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:boorusama/core/application/tags/tags.dart';
+import 'package:boorusama/core/domain/tags/favorite_tag.dart';
+import 'package:boorusama/core/domain/tags/favorite_tag_repository.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockFavoriteTagRepository extends Mock implements FavoriteTagRepository {}
+
+void main() {
+  final favTagRepo = MockFavoriteTagRepository();
+
+  blocTest<FavoriteTagBloc, FavoriteTagState>(
+    'fetch all tags',
+    setUp: () {
+      when(() => favTagRepo.getAll()).thenAnswer((invocation) async => [
+            FavoriteTag.empty(),
+          ]);
+    },
+    tearDown: () {
+      reset(favTagRepo);
+    },
+    build: () => FavoriteTagBloc(
+      favoriteTagRepository: favTagRepo,
+    ),
+    act: (bloc) => bloc.add(const FavoriteTagFetched()),
+    expect: () => [
+      FavoriteTagState.initial().copyWith(tags: [
+        FavoriteTag.empty(),
+      ]),
+    ],
+  );
+
+  blocTest<FavoriteTagBloc, FavoriteTagState>(
+    'add tag',
+    setUp: () {
+      when(() => favTagRepo.getAll()).thenAnswer((invocation) async => [
+            FavoriteTag.empty().copyWith(name: 'foo'),
+          ]);
+      when(() => favTagRepo.create(name: any(named: 'name'))).thenAnswer(
+        (invocation) async => FavoriteTag.empty().copyWith(name: 'foo'),
+      );
+    },
+    tearDown: () {
+      reset(favTagRepo);
+    },
+    build: () => FavoriteTagBloc(
+      favoriteTagRepository: favTagRepo,
+    ),
+    act: (bloc) => bloc.add(const FavoriteTagAdded(tag: 'foo')),
+    verify: (bloc) =>
+        verify(() => favTagRepo.create(name: any(named: 'name'))).called(1),
+    expect: () => [
+      FavoriteTagState.initial().copyWith(tags: [
+        FavoriteTag.empty().copyWith(name: 'foo'),
+      ]),
+    ],
+  );
+
+  blocTest<FavoriteTagBloc, FavoriteTagState>(
+    'remove tag',
+    setUp: () {
+      when(() => favTagRepo.getAll()).thenAnswer((invocation) async => []);
+      when(() => favTagRepo.deleteFirst(any())).thenAnswer(
+        (invocation) async => FavoriteTag.empty().copyWith(name: 'foo'),
+      );
+    },
+    tearDown: () {
+      reset(favTagRepo);
+    },
+    seed: () => FavoriteTagState.initial().copyWith(tags: [
+      FavoriteTag.empty().copyWith(name: 'foo'),
+    ]),
+    build: () => FavoriteTagBloc(
+      favoriteTagRepository: favTagRepo,
+    ),
+    act: (bloc) => bloc.add(const FavoriteTagRemoved(index: 0)),
+    verify: (bloc) => verify(() => favTagRepo.deleteFirst(any())).called(1),
+    expect: () => [
+      FavoriteTagState.initial(),
+    ],
+  );
+
+  blocTest<FavoriteTagBloc, FavoriteTagState>(
+    'import tag',
+    setUp: () {
+      when(() => favTagRepo.getAll()).thenAnswer((invocation) async => [
+            FavoriteTag.empty().copyWith(name: '0'),
+            FavoriteTag.empty().copyWith(name: '1'),
+            FavoriteTag.empty().copyWith(name: '2'),
+          ]);
+      when(() => favTagRepo.create(name: any(named: 'name'))).thenAnswer(
+        (invocation) async => FavoriteTag.empty().copyWith(name: '0'),
+      );
+    },
+    tearDown: () {
+      reset(favTagRepo);
+    },
+    seed: () => FavoriteTagState.initial().copyWith(tags: [
+      FavoriteTag.empty().copyWith(name: '0'),
+    ]),
+    build: () => FavoriteTagBloc(
+      favoriteTagRepository: favTagRepo,
+    ),
+    act: (bloc) => bloc.add(const FavoriteTagImported(tagString: '1 2')),
+    verify: (bloc) =>
+        verify(() => favTagRepo.create(name: any(named: 'name'))).called(2),
+    expect: () => [
+      FavoriteTagState.initial().copyWith(tags: [
+        FavoriteTag.empty().copyWith(name: '0'),
+        FavoriteTag.empty().copyWith(name: '1'),
+        FavoriteTag.empty().copyWith(name: '2'),
+      ]),
+    ],
+  );
+}
