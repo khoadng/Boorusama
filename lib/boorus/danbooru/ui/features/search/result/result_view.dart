9881fea5fc9154c1c4d7aae3adb6ff80fcbc53c0
khoa.nguyen <nguyenduckhoa1996@gmail.com>
2022-11-22 20:56:41 +0700

    feat: extract widgets

--- lib/boorus/danbooru/ui/features/search/result/result_view.dart
+++ lib/boorus/danbooru/ui/features/search/result/result_view.dart
@@ -3,9 +3,11 @@ import 'dart:math' as math;
 
 // Flutter imports:
 import 'package:flutter/material.dart' hide ThemeMode;
+import 'package:flutter/rendering.dart';
 
 // Package imports:
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart' hide LoadStatus;
 import 'package:scroll_to_index/scroll_to_index.dart';
 
@@ -38,6 +40,197 @@ class _ResultViewState extends State<ResultView> {
     super.dispose();
   }
 
+  @override
+  Widget build(BuildContext context) {
+    final pagination = context.select((PostBloc bloc) => bloc.state.pagination);
+
+    return pagination
+        ? _Pagination(
+            scrollController: scrollController,
+          )
+        : _InfiniteScroll(
+            scrollController: scrollController,
+            refreshController: refreshController,
+          );
+  }
+}
+
+class _InfiniteScroll extends StatelessWidget {
+  const _InfiniteScroll({
+    required this.scrollController,
+    required this.refreshController,
+  });
+
+  final AutoScrollController scrollController;
+  final RefreshController refreshController;
+
+  @override
+  Widget build(BuildContext context) {
+    final tags = context.select((SearchBloc bloc) => bloc.state.selectedTags);
+    final state = context.watch<PostBloc>().state;
+
+    return InfiniteLoadListScrollView(
+      scrollController: scrollController,
+      isLoading: state.loading,
+      refreshController: refreshController,
+      enableLoadMore: state.hasMore,
+      onLoadMore: () => context.read<PostBloc>().add(PostFetched(
+            tags: tags.map((e) => e.toString()).join(' '),
+            fetcher: SearchedPostFetcher.fromTags(
+              tags.map((e) => e.toString()).join(' '),
+            ),
+          )),
+      onRefresh: (controller) {
+        context.read<PostBloc>().add(PostRefreshed(
+              tag: tags.map((e) => e.toString()).join(' '),
+              fetcher: SearchedPostFetcher.fromTags(
+                tags.map((e) => e.toString()).join(' '),
+              ),
+            ));
+        Future.delayed(
+          const Duration(milliseconds: 500),
+          () => controller.refreshCompleted(),
+        );
+      },
+      sliverBuilder: (controller) => [
+        SliverToBoxAdapter(
+          child: SizedBox(height: MediaQuery.of(context).viewPadding.top),
+        ),
+        const SliverToBoxAdapter(child: RelatedTagSection()),
+        const SliverToBoxAdapter(child: ResultHeader()),
+        HomePostGrid(
+          controller: controller,
+          onTap: () => FocusScope.of(context).unfocus(),
+        ),
+      ],
+    );
+  }
+}
+
+class _Pagination extends StatefulWidget {
+  const _Pagination({
+    required this.scrollController,
+  });
+
+  final AutoScrollController scrollController;
+
+  @override
+  State<_Pagination> createState() => _PaginationState();
+}
+
+class _PaginationState extends State<_Pagination>
+    with TickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: kThemeAnimationDuration,
+      reverseDuration: kThemeAnimationDuration,
+    );
+
+    widget.scrollController.addListener(_onScroll);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _animationController.dispose();
+    widget.scrollController.removeListener(_onScroll);
+  }
+
+  void _onScroll() {
+    switch (widget.scrollController.position.userScrollDirection) {
+      case ScrollDirection.forward:
+        _animationController.forward();
+        break;
+      case ScrollDirection.reverse:
+        _animationController.reverse();
+        break;
+      case ScrollDirection.idle:
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final tags = context.select((SearchBloc bloc) => bloc.state.selectedTags);
+    final totalResults =
+        context.select((SearchBloc bloc) => bloc.state.totalResults);
+    final maxPage = (totalResults / PostBloc.postPerPage).ceil();
+    final state = context.watch<PostBloc>().state;
+
+    return Scaffold(
+      floatingActionButton: FadeTransition(
+        opacity: _animationController,
+        child: ScaleTransition(
+          scale: _animationController,
+          child: FloatingActionButton(
+            heroTag: null,
+            child: const FaIcon(FontAwesomeIcons.angleUp),
+            onPressed: () => widget.scrollController.jumpTo(0),
+          ),
+        ),
+      ),
+      body: CustomScrollView(
+        controller: widget.scrollController,
+        slivers: [
+          SliverToBoxAdapter(
+            child: SizedBox(height: MediaQuery.of(context).viewPadding.top),
+          ),
+          const SliverToBoxAdapter(child: RelatedTagSection()),
+          const SliverToBoxAdapter(child: ResultHeader()),
+          HomePostGrid(
+            controller: widget.scrollController,
+            onTap: () => FocusScope.of(context).unfocus(),
+          ),
+          const SliverToBoxAdapter(child: SizedBox(height: 20)),
+          SliverToBoxAdapter(
+            child: ButtonBar(
+              buttonPadding: const EdgeInsets.symmetric(horizontal: 2),
+              alignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  onPressed: state.page > 1
+                      ? () => _fetch(state.page - 1, tags)
+                      : null,
+                  icon: const Icon(Icons.chevron_left),
+                ),
+                ...generatePage(
+                  current: state.page,
+                  total: totalResults,
+                  postPerPage: PostBloc.postPerPage,
+                ).map((page) => ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: const CircleBorder(),
+                        backgroundColor: page == state.page
+                            ? Theme.of(context).colorScheme.primary
+                            : Colors.transparent,
+                      ),
+                      onPressed: () {
+                        _fetch(page, tags);
+                      },
+                      child: Text(
+                        '$page',
+                        style: Theme.of(context).textTheme.titleLarge,
+                      ),
+                    )),
+                IconButton(
+                  onPressed: maxPage != state.page
+                      ? () => _fetch(state.page + 1, tags)
+                      : null,
+                  icon: const Icon(Icons.chevron_right),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   void _fetch(int page, List<TagSearchItem> tags) {
     context.read<PostBloc>().add(PostFetched(
           page: page,
@@ -46,107 +239,7 @@ class _ResultViewState extends State<ResultView> {
             tags.map((e) => e.toString()).join(' '),
           ),
         ));
-    scrollController.jumpTo(0);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final tags = context.select((SearchBloc bloc) => bloc.state.selectedTags);
-    final totalResults =
-        context.select((SearchBloc bloc) => bloc.state.totalResults);
-    final state = context.watch<PostBloc>().state;
-    final maxPage = (totalResults / PostBloc.postPerPage).ceil();
-
-    return state.pagination
-        ? CustomScrollView(
-            controller: scrollController,
-            slivers: [
-              SliverToBoxAdapter(
-                child: SizedBox(height: MediaQuery.of(context).viewPadding.top),
-              ),
-              const SliverToBoxAdapter(child: RelatedTagSection()),
-              const SliverToBoxAdapter(child: ResultHeader()),
-              HomePostGrid(
-                controller: scrollController,
-                onTap: () => FocusScope.of(context).unfocus(),
-              ),
-              const SliverToBoxAdapter(child: SizedBox(height: 20)),
-              SliverToBoxAdapter(
-                child: ButtonBar(
-                  buttonPadding: const EdgeInsets.symmetric(horizontal: 2),
-                  alignment: MainAxisAlignment.center,
-                  children: [
-                    IconButton(
-                      onPressed: state.page > 1
-                          ? () => _fetch(state.page - 1, tags)
-                          : null,
-                      icon: const Icon(Icons.chevron_left),
-                    ),
-                    ...generatePage(
-                      current: state.page,
-                      total: totalResults,
-                      postPerPage: PostBloc.postPerPage,
-                    ).map((page) => ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            shape: const CircleBorder(),
-                            backgroundColor: page == state.page
-                                ? Theme.of(context).colorScheme.primary
-                                : Colors.transparent,
-                          ),
-                          onPressed: () {
-                            _fetch(page, tags);
-                          },
-                          child: Text(
-                            '$page',
-                            style: Theme.of(context).textTheme.titleLarge,
-                          ),
-                        )),
-                    IconButton(
-                      onPressed: maxPage != state.page
-                          ? () => _fetch(state.page + 1, tags)
-                          : null,
-                      icon: const Icon(Icons.chevron_right),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          )
-        : InfiniteLoadListScrollView(
-            scrollController: scrollController,
-            isLoading: state.loading,
-            refreshController: refreshController,
-            enableLoadMore: state.hasMore,
-            onLoadMore: () => context.read<PostBloc>().add(PostFetched(
-                  tags: tags.map((e) => e.toString()).join(' '),
-                  fetcher: SearchedPostFetcher.fromTags(
-                    tags.map((e) => e.toString()).join(' '),
-                  ),
-                )),
-            onRefresh: (controller) {
-              context.read<PostBloc>().add(PostRefreshed(
-                    tag: tags.map((e) => e.toString()).join(' '),
-                    fetcher: SearchedPostFetcher.fromTags(
-                      tags.map((e) => e.toString()).join(' '),
-                    ),
-                  ));
-              Future.delayed(
-                const Duration(milliseconds: 500),
-                () => controller.refreshCompleted(),
-              );
-            },
-            sliverBuilder: (controller) => [
-              SliverToBoxAdapter(
-                child: SizedBox(height: MediaQuery.of(context).viewPadding.top),
-              ),
-              const SliverToBoxAdapter(child: RelatedTagSection()),
-              const SliverToBoxAdapter(child: ResultHeader()),
-              HomePostGrid(
-                controller: controller,
-                onTap: () => FocusScope.of(context).unfocus(),
-              ),
-            ],
-          );
+    widget.scrollController.jumpTo(0);
   }
 }
 
