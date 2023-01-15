e144bf46151b7e23c1f78c69c12ad53339bbed25
khoa.nguyen <nguyenduckhoa1996@gmail.com>
2023-01-15 17:10:13 +0700

    test(common): add tests for queue and list extensions

---
+++ test/list_extension_test.dart
@@ -0,0 +1,42 @@
+import 'package:boorusama/common/collection_utils.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('[getOrNull]', () {
+    test('getOrNull returns element', () {
+      final list = <int>[1];
+      expect(list.getOrNull(0), 1);
+    });
+
+    test('getOrNull returns null', () {
+      final list = <int>[];
+      expect(list.getOrNull(0), null);
+    });
+
+    test('getOrNull returns null for out of range index', () {
+      final list = <int>[1];
+      expect(list.getOrNull(1), null);
+    });
+    test('getOrNull returns null for negative index', () {
+      final list = <int>[1];
+      expect(list.getOrNull(-1), null);
+    });
+  });
+
+  group('[firstOrNull]', () {
+    test('firstOrNull returns first element that matches selector', () {
+      final list = [1, 2, 3, 4, 5];
+      expect(list.firstOrNull((e) => e.isEven), 2);
+    });
+
+    test('firstOrNull returns null for empty list', () {
+      final list = <int>[];
+      expect(list.firstOrNull((e) => e.isEven), null);
+    });
+
+    test('firstOrNull returns null for no matching elements', () {
+      final list = [1, 3, 5];
+      expect(list.firstOrNull((e) => e.isEven), null);
+    });
+  });
+}
