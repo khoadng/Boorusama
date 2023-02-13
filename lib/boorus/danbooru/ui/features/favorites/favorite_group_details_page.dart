c9b1383fbf842939965937db8640fc62e3ccc9c9
khoa.nguyen <nguyenduckhoa1996@gmail.com>
2023-02-13 19:44:29 +0700

    update

--- lib/boorus/danbooru/ui/features/favorites/favorite_group_details_page.dart
+++ lib/boorus/danbooru/ui/features/favorites/favorite_group_details_page.dart
@@ -2,8 +2,10 @@
 import 'dart:collection';
 
 // Flutter imports:
+import 'package:boorusama/boorus/danbooru/ui/features/post_detail/widgets/circular_icon_button.dart';
 import 'package:boorusama/core/core.dart';
 import 'package:boorusama/core/ui/image_grid_item.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 // Package imports:
@@ -35,16 +37,23 @@ class FavoriteGroupDetailsPage extends StatefulWidget {
 }
 
 class _FavoriteGroupDetailsPageState extends State<FavoriteGroupDetailsPage> {
-  bool changed = false;
   List<List<Object>> commands = [];
+  bool editing = false;
 
   void _aggregate() {
     final ids = widget.group.postIds;
     for (final cmd in commands) {
-      final toIndex = cmd[1] as int;
-      final fromIndex = cmd.first as int;
-      final item = ids.removeAt(fromIndex);
-      ids.insert(toIndex, item);
+      final toIndex = cmd[2] as int;
+      final fromIndex = cmd[1] as int;
+      final deleteCmd = cmd.first as bool;
+      final deleteTarget = cmd[3] as int;
+
+      if (deleteCmd) {
+        ids.remove(deleteTarget);
+      } else {
+        final item = ids.removeAt(fromIndex);
+        ids.insert(toIndex, item);
+      }
     }
 
     context.read<FavoriteGroupsBloc>().add(FavoriteGroupsEdited(
@@ -58,102 +67,164 @@ class _FavoriteGroupDetailsPageState extends State<FavoriteGroupDetailsPage> {
     final state = context.select((PostBloc bloc) => bloc.state);
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.group.name.replaceAll('_', ' ')),
-        elevation: 0,
-        shadowColor: Colors.transparent,
-        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-        actions: [
-          IconButton(
-            onPressed: () {
-              goToSearchPage(
-                context,
-                tag: widget.group.getQueryString(),
-              );
-            },
-            icon: const Icon(Icons.search),
-          ),
-          IconButton(
-            onPressed: () {
-              goToBulkDownloadPage(
-                context,
-                [widget.group.getQueryString()],
-              );
-            },
-            icon: const Icon(Icons.download),
-          ),
-        ],
-      ),
-      floatingActionButton: changed
+      floatingActionButton: editing
           ? FloatingActionButton(
               onPressed: () {
                 _aggregate();
                 setState(() {
-                  changed = false;
+                  editing = false;
                 });
               },
               child: const Icon(Icons.save),
             )
           : null,
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text(widget.group.name.replaceAll('_', ' ')),
+        elevation: 0,
+        shadowColor: Colors.transparent,
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        actions: [
+          if (!editing)
+            IconButton(
+              onPressed: () {
+                goToSearchPage(
+                  context,
+                  tag: widget.group.getQueryString(),
+                );
+              },
+              icon: const Icon(Icons.search),
+            ),
+          if (!editing)
+            IconButton(
+              onPressed: () {
+                goToBulkDownloadPage(
+                  context,
+                  [widget.group.getQueryString()],
+                );
+              },
+              icon: const Icon(Icons.download),
+            ),
+          if (!editing)
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  editing = true;
+                  commands.clear();
+                });
+              },
+              icon: const Icon(Icons.edit),
+            )
+          else
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  editing = false;
+                  commands.clear();
+                });
+              },
+              child: const Text('generic.action.cancel').tr(),
+            ),
+        ],
+      ),
       body: state.refreshing
           ? const Center(
               child: CircularProgressIndicator(),
             )
-          : InfiniteLoadList(
-              onLoadMore: () => context.read<PostBloc>().add(PostFetched(
-                    tags: '',
-                    fetcher: FavoriteGroupPostFetcher(
-                      ids: widget.postIds.dequeue(20),
-                    ),
-                  )),
-              enableRefresh: false,
-              enableLoadMore: state.hasMore,
-              builder: (context, controller) {
-                return ReorderableGridView.builder(
-                  controller: controller,
-                  itemCount: state.data.length,
-                  onReorder: (oldIndex, newIndex) =>
-                      context.read<PostBloc>().add(PostMovedAndInserted(
-                            fromIndex: oldIndex,
-                            toIndex: newIndex,
-                            onSuccess: () {
-                              setState(() {
-                                commands.add([oldIndex, newIndex]);
-                                changed = true;
-                              });
-                            },
-                          )),
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: _sizeToGridCount(Screen.of(context).size),
-                    mainAxisSpacing: 4,
-                    crossAxisSpacing: 4,
+          : Column(
+              children: [
+                if (editing)
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 4),
+                    child: Text('Drag and drop to determine ordering.'),
                   ),
-                  itemBuilder: (context, index) {
-                    final post = state.data[index];
+                Expanded(
+                  child: InfiniteLoadList(
+                    onLoadMore: () => context.read<PostBloc>().add(PostFetched(
+                          tags: '',
+                          fetcher: FavoriteGroupPostFetcher(
+                            ids: widget.postIds.dequeue(20),
+                          ),
+                        )),
+                    enableRefresh: false,
+                    enableLoadMore: state.hasMore,
+                    builder: (context, controller) {
+                      return ReorderableGridView.builder(
+                        controller: controller,
+                        dragEnabled: editing,
+                        itemCount: state.data.length,
+                        onReorder: (oldIndex, newIndex) => context
+                            .read<PostBloc>()
+                            .add(PostMovedAndInserted(
+                              fromIndex: oldIndex,
+                              toIndex: newIndex,
+                              onSuccess: () {
+                                if (oldIndex != newIndex) {
+                                  setState(() {
+                                    commands
+                                        .add([false, oldIndex, newIndex, 0]);
+                                  });
+                                }
+                              },
+                            )),
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount:
+                              _sizeToGridCount(Screen.of(context).size),
+                          mainAxisSpacing: 4,
+                          crossAxisSpacing: 4,
+                        ),
+                        itemBuilder: (context, index) {
+                          final post = state.data[index];
 
-                    return ImageGridItem(
-                      key: ValueKey(index),
-                      onTap: () => goToDetailPage(
-                        context: context,
-                        posts: state.data,
-                        initialIndex: index,
-                      ),
-                      image: BooruImage(
-                        fit: BoxFit.cover,
-                        imageUrl: post.post.isAnimated
-                            ? post.post.previewImageUrl
-                            : post.post.normalImageUrl,
-                        placeholderUrl: post.post.previewImageUrl,
-                        aspectRatio: 1,
-                      ),
-                      isAnimated: post.post.isAnimated,
-                      isTranslated: post.post.isTranslated,
-                      hasComments: post.post.hasComment,
-                      hasParentOrChildren: post.post.hasParentOrChildren,
-                    );
-                  },
-                );
-              },
+                          return Stack(
+                            key: ValueKey(index),
+                            children: [
+                              ImageGridItem(
+                                onTap: !editing
+                                    ? () => goToDetailPage(
+                                          context: context,
+                                          posts: state.data,
+                                          initialIndex: index,
+                                        )
+                                    : null,
+                                image: BooruImage(
+                                  fit: BoxFit.cover,
+                                  imageUrl: post.post.isAnimated
+                                      ? post.post.previewImageUrl
+                                      : post.post.normalImageUrl,
+                                  placeholderUrl: post.post.previewImageUrl,
+                                  aspectRatio: 1,
+                                ),
+                                isAnimated: post.post.isAnimated,
+                                isTranslated: post.post.isTranslated,
+                                hasComments: post.post.hasComment,
+                                hasParentOrChildren:
+                                    post.post.hasParentOrChildren,
+                              ),
+                              if (editing)
+                                Positioned(
+                                  top: 8,
+                                  right: 8,
+                                  child: CircularIconButton(
+                                    icon: const Icon(Icons.close),
+                                    onPressed: () {
+                                      context.read<PostBloc>().add(
+                                            PostRemoved(
+                                              postIds: [post.post.id],
+                                            ),
+                                          );
+                                      commands.add([true, 0, 0, post.post.id]);
+                                    },
+                                  ),
+                                ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
     );
   }
