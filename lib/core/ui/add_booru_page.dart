31c5ced2713dad6c2c110493f5374c0c6307339e
khoa.nguyen <nguyenduckhoa1996@gmail.com>
2023-03-27 23:52:32 +0700

    add out of focus

--- lib/core/ui/add_booru_page.dart
+++ lib/core/ui/add_booru_page.dart
@@ -86,176 +86,179 @@ class _AddBooruPageState extends State<AddBooruPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        backgroundColor: Colors.transparent,
-        shadowColor: Colors.transparent,
-        elevation: 0,
-        leading: IconButton(
-          onPressed: Navigator.of(context).pop,
-          icon: const Icon(Icons.close),
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          backgroundColor: Colors.transparent,
+          shadowColor: Colors.transparent,
+          elevation: 0,
+          leading: IconButton(
+            onPressed: Navigator.of(context).pop,
+            icon: const Icon(Icons.close),
+          ),
         ),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 12,
-                vertical: 16,
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 16,
+                ),
+                child: Text(
+                  'Add a Booru, leave the login details empty to be anonymous',
+                  style: Theme.of(context)
+                      .textTheme
+                      .headlineSmall!
+                      .copyWith(fontWeight: FontWeight.w900),
+                ),
               ),
-              child: Text(
-                'Add a Booru, leave the login details empty to be anonymous',
-                style: Theme.of(context)
-                    .textTheme
-                    .headlineSmall!
-                    .copyWith(fontWeight: FontWeight.w900),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: DropdownButtonHideUnderline(
-                    child: DropdownButton<BooruType>(
-                      isDense: true,
-                      value: selectedBooru,
-                      focusColor: Colors.transparent,
-                      icon: const Padding(
-                        padding: EdgeInsets.only(left: 5, top: 2),
-                        child: FaIcon(FontAwesomeIcons.angleDown, size: 16),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: DropdownButtonHideUnderline(
+                      child: DropdownButton<BooruType>(
+                        isDense: true,
+                        value: selectedBooru,
+                        focusColor: Colors.transparent,
+                        icon: const Padding(
+                          padding: EdgeInsets.only(left: 5, top: 2),
+                          child: FaIcon(FontAwesomeIcons.angleDown, size: 16),
+                        ),
+                        onChanged: (newValue) {
+                          if (newValue != null) {
+                            setState(() {
+                              selectedBooru = newValue;
+                            });
+                          }
+                        },
+                        items: getSelectableBoorus()
+                            .sortedBy((e) => e.stringify())
+                            .map((value) => DropdownMenuItem<BooruType>(
+                                  value: value,
+                                  child: Text(value.stringify()),
+                                ))
+                            .toList(),
                       ),
-                      onChanged: (newValue) {
-                        if (newValue != null) {
-                          setState(() {
-                            selectedBooru = newValue;
-                          });
-                        }
-                      },
-                      items: getSelectableBoorus()
-                          .sortedBy((e) => e.stringify())
-                          .map((value) => DropdownMenuItem<BooruType>(
-                                value: value,
-                                child: Text(value.stringify()),
-                              ))
-                          .toList(),
                     ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(height: 8),
-            const Divider(
-              thickness: 2,
-              endIndent: 16,
-              indent: 16,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Text(
-                'Name'.toUpperCase(),
-                style: Theme.of(context).textTheme.titleSmall!.copyWith(
-                      color: Theme.of(context).hintColor,
-                      fontWeight: FontWeight.w800,
+              const SizedBox(height: 8),
+              const Divider(
+                thickness: 2,
+                endIndent: 16,
+                indent: 16,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Text(
+                  'Name'.toUpperCase(),
+                  style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                        color: Theme.of(context).hintColor,
+                        fontWeight: FontWeight.w800,
+                      ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 8,
+                ),
+                child: LoginField(
+                  validator: (p0) => null,
+                  controller: nameController,
+                  labelText: 'Name',
+                ),
+              ),
+              const SizedBox(height: 8),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Text(
+                  'Login details'.toUpperCase(),
+                  style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                        color: Theme.of(context).hintColor,
+                        fontWeight: FontWeight.w800,
+                      ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 8,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    LoginField(
+                      validator: (p0) => null,
+                      controller: loginController,
+                      labelText: 'Login',
                     ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 16,
-                vertical: 8,
-              ),
-              child: LoginField(
-                validator: (p0) => null,
-                controller: nameController,
-                labelText: 'Name',
-              ),
-            ),
-            const SizedBox(height: 8),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Text(
-                'Login details'.toUpperCase(),
-                style: Theme.of(context).textTheme.titleSmall!.copyWith(
-                      color: Theme.of(context).hintColor,
-                      fontWeight: FontWeight.w800,
+                    const SizedBox(height: 16),
+                    LoginField(
+                      validator: (p0) => null,
+                      obscureText: !showKey,
+                      controller: apiKeyController,
+                      labelText: 'API key',
+                      suffixIcon: IconButton(
+                        splashColor: Colors.transparent,
+                        icon: showKey
+                            ? const FaIcon(
+                                FontAwesomeIcons.solidEyeSlash,
+                                size: 18,
+                              )
+                            : const FaIcon(
+                                FontAwesomeIcons.solidEye,
+                                size: 18,
+                              ),
+                        onPressed: () => setState(() => showKey = !showKey),
+                      ),
                     ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 16,
-                vertical: 8,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  LoginField(
-                    validator: (p0) => null,
-                    controller: loginController,
-                    labelText: 'Login',
-                  ),
-                  const SizedBox(height: 16),
-                  LoginField(
-                    validator: (p0) => null,
-                    obscureText: !showKey,
-                    controller: apiKeyController,
-                    labelText: 'API key',
-                    suffixIcon: IconButton(
-                      splashColor: Colors.transparent,
-                      icon: showKey
-                          ? const FaIcon(
-                              FontAwesomeIcons.solidEyeSlash,
-                              size: 18,
-                            )
-                          : const FaIcon(
-                              FontAwesomeIcons.solidEye,
-                              size: 18,
-                            ),
-                      onPressed: () => setState(() => showKey = !showKey),
+                    const SizedBox(height: 16),
+                    if (selectedBooru != BooruType.safebooru)
+                      SwitchListTile.adaptive(
+                        title: const Text('Rating filter'),
+                        value: ratingFilter,
+                        onChanged: (value) => setState(() {
+                          ratingFilter = value;
+                        }),
+                      ),
+                    if (selectedBooru != BooruType.gelbooru)
+                      SwitchListTile.adaptive(
+                        title: const Text('Hide deleted posts'),
+                        value: hideDeleted,
+                        onChanged: (value) => setState(() {
+                          hideDeleted = value;
+                        }),
+                      ),
+                    ElevatedButton(
+                      onPressed: allowSubmit
+                          ? () {
+                              Navigator.of(context).pop();
+                              widget.onSubmit.call(AddNewBooruConfig(
+                                login: loginController.text,
+                                apiKey: apiKeyController.text,
+                                booru: selectedBooru,
+                                configName: nameController.text,
+                                hideDeleted: hideDeleted,
+                                ratingFilter: ratingFilter,
+                              ));
+                            }
+                          : null,
+                      child: const Text('OK'),
                     ),
-                  ),
-                  const SizedBox(height: 16),
-                  if (selectedBooru != BooruType.safebooru)
-                    SwitchListTile.adaptive(
-                      title: const Text('Rating filter'),
-                      value: ratingFilter,
-                      onChanged: (value) => setState(() {
-                        ratingFilter = value;
-                      }),
-                    ),
-                  if (selectedBooru != BooruType.gelbooru)
-                    SwitchListTile.adaptive(
-                      title: const Text('Hide deleted posts'),
-                      value: hideDeleted,
-                      onChanged: (value) => setState(() {
-                        hideDeleted = value;
-                      }),
-                    ),
-                  ElevatedButton(
-                    onPressed: allowSubmit
-                        ? () {
-                            Navigator.of(context).pop();
-                            widget.onSubmit.call(AddNewBooruConfig(
-                              login: loginController.text,
-                              apiKey: apiKeyController.text,
-                              booru: selectedBooru,
-                              configName: nameController.text,
-                              hideDeleted: hideDeleted,
-                              ratingFilter: ratingFilter,
-                            ));
-                          }
-                        : null,
-                    child: const Text('OK'),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
