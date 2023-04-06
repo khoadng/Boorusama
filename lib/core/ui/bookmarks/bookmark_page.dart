0cdc764c7277a1a29c80369dd6e2026b20fcd985
khoa.nguyen <nguyenduckhoa1996@gmail.com>
2023-04-06 11:58:27 +0700

    allow user to interact with bookmark page

--- lib/core/ui/bookmarks/bookmark_page.dart
+++ lib/core/ui/bookmarks/bookmark_page.dart
@@ -7,8 +7,12 @@ import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 // Project imports:
 import 'package:boorusama/core/application/bookmarks/bookmark_cubit.dart';
+import 'package:boorusama/core/domain/boorus.dart';
 import 'package:boorusama/core/router.dart';
 import 'package:boorusama/core/ui/booru_image.dart';
+import 'package:boorusama/core/ui/booru_logo.dart';
+import 'package:boorusama/core/ui/circular_icon_button.dart';
+import 'package:boorusama/core/ui/editable_mixin.dart';
 
 class BookmarkPage extends StatefulWidget {
   const BookmarkPage({Key? key}) : super(key: key);
@@ -17,52 +21,118 @@ class BookmarkPage extends StatefulWidget {
   _BookmarkPageState createState() => _BookmarkPageState();
 }
 
-class _BookmarkPageState extends State<BookmarkPage> {
+class _BookmarkPageState extends State<BookmarkPage> with EditableMixin {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Bookmarks'),
-      ),
-      body: BlocBuilder<BookmarkCubit, BookmarkState>(
-        builder: (context, state) {
-          switch (state.status) {
-            case BookmarkStatus.loading:
-              return Center(child: CircularProgressIndicator());
-            case BookmarkStatus.success:
-              return CustomScrollView(
-                slivers: [
-                  SliverMasonryGrid.count(
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 10,
-                    crossAxisSpacing: 10,
-                    childCount: state.bookmarks.length,
-                    itemBuilder: (context, index) {
-                      final bookmark = state.bookmarks[index];
+    final factory = context.read<BooruFactory>();
 
-                      return GestureDetector(
-                        onTap: () => goToBookmarkDetailsPage(
-                          context: context,
-                          bookmarks: state.bookmarks,
-                          initialIndex: index,
-                        ),
-                        child: BooruImage(
-                          aspectRatio: bookmark.aspectRatio,
-                          fit: BoxFit.cover,
-                          imageUrl: bookmark.sampleUrl,
-                          placeholderUrl: bookmark.thumbnailUrl,
-                        ),
-                      );
-                    },
-                  )
-                ],
-              );
-            case BookmarkStatus.failure:
-              return Center(child: Text(state.error));
-            default:
-              return Container();
-          }
-        },
+    return WillPopScope(
+      onWillPop: () async {
+        if (edit) {
+          endEditMode();
+          return false;
+        }
+
+        return true;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Bookmarks'),
+          automaticallyImplyLeading: !edit,
+          leading: edit
+              ? IconButton(
+                  onPressed: () => endEditMode(),
+                  icon: Icon(
+                    Icons.check,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                )
+              : null,
+          actions: [
+            if (!edit)
+              PopupMenuButton<String>(
+                onSelected: (value) {
+                  switch (value) {
+                    case 'edit':
+                      startEditMode();
+                      break;
+                    default:
+                  }
+                },
+                itemBuilder: (BuildContext context) {
+                  return [
+                    PopupMenuItem(
+                      value: 'edit',
+                      child: const Text('Edit'),
+                    ),
+                  ];
+                },
+              ),
+          ],
+        ),
+        body: BlocBuilder<BookmarkCubit, BookmarkState>(
+          builder: (context, state) {
+            switch (state.status) {
+              case BookmarkStatus.loading:
+                return Center(child: CircularProgressIndicator());
+              case BookmarkStatus.success:
+                return CustomScrollView(
+                  slivers: [
+                    SliverMasonryGrid.count(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 10,
+                      crossAxisSpacing: 10,
+                      childCount: state.bookmarks.length,
+                      itemBuilder: (context, index) {
+                        final bookmark = state.bookmarks[index];
+
+                        return GestureDetector(
+                          onTap: () => goToBookmarkDetailsPage(
+                            context: context,
+                            bookmarks: state.bookmarks,
+                            initialIndex: index,
+                          ),
+                          child: Stack(
+                            children: [
+                              BooruImage(
+                                aspectRatio: bookmark.aspectRatio,
+                                fit: BoxFit.cover,
+                                imageUrl: bookmark.sampleUrl,
+                                placeholderUrl: bookmark.thumbnailUrl,
+                              ),
+                              Positioned(
+                                top: 5,
+                                left: 5,
+                                child: BooruLogo(
+                                    booru: factory.from(
+                                        type:
+                                            intToBooruType(bookmark.booruId))),
+                              ),
+                              if (edit)
+                                Positioned(
+                                  top: 5,
+                                  right: 5,
+                                  child: CircularIconButton(
+                                    icon: Icon(Icons.close),
+                                    onPressed: () => context
+                                        .read<BookmarkCubit>()
+                                        .removeBookmark(bookmark),
+                                  ),
+                                ),
+                            ],
+                          ),
+                        );
+                      },
+                    )
+                  ],
+                );
+              case BookmarkStatus.failure:
+                return Center(child: Text(state.error));
+              default:
+                return Container();
+            }
+          },
+        ),
       ),
     );
   }
