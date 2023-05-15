9f923098fdbee23425e5ef90e0bf10e65e237c7c
khoa.nguyen <nguyenduckhoa1996@gmail.com>
2023-05-15 19:55:12 +0700

    fix empty

--- lib/boorus/danbooru/ui/home/most_search_tag_list.dart
+++ lib/boorus/danbooru/ui/home/most_search_tag_list.dart
@@ -24,48 +24,51 @@ class MostSearchTagList extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final asyncData = ref.watch(trendingTagsProvider);
 
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-      height: 40,
-      child: asyncData.when(
-        data: (searches) => ListView.builder(
-          shrinkWrap: true,
-          scrollDirection: Axis.horizontal,
-          itemCount: searches.length,
-          itemBuilder: (context, index) {
-            final isSelected = selected == searches[index].keyword;
+    return asyncData.when(
+      data: (searches) => searches.isNotEmpty
+          ? Container(
+              margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+              height: 40,
+              child: ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: searches.length,
+                itemBuilder: (context, index) {
+                  final isSelected = selected == searches[index].keyword;
 
-            return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 4),
-              child: ChoiceChip(
-                disabledColor: Theme.of(context).chipTheme.disabledColor,
-                backgroundColor: Theme.of(context).chipTheme.backgroundColor,
-                selectedColor: Theme.of(context).chipTheme.selectedColor,
-                selected: isSelected,
-                onSelected: (selected) => onSelected(searches[index]),
-                padding: const EdgeInsets.all(4),
-                labelPadding: const EdgeInsets.all(1),
-                visualDensity: VisualDensity.compact,
-                side: BorderSide(
-                  width: 0.5,
-                  color: Theme.of(context).hintColor,
-                ),
-                label: ConstrainedBox(
-                  constraints: BoxConstraints(
-                    maxWidth: MediaQuery.of(context).size.width * 0.85,
-                  ),
-                  child: Text(
-                    searches[index].keyword.removeUnderscoreWithSpace(),
-                    overflow: TextOverflow.fade,
-                  ),
-                ),
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 4),
+                    child: ChoiceChip(
+                      disabledColor: Theme.of(context).chipTheme.disabledColor,
+                      backgroundColor:
+                          Theme.of(context).chipTheme.backgroundColor,
+                      selectedColor: Theme.of(context).chipTheme.selectedColor,
+                      selected: isSelected,
+                      onSelected: (selected) => onSelected(searches[index]),
+                      padding: const EdgeInsets.all(4),
+                      labelPadding: const EdgeInsets.all(1),
+                      visualDensity: VisualDensity.compact,
+                      side: BorderSide(
+                        width: 0.5,
+                        color: Theme.of(context).hintColor,
+                      ),
+                      label: ConstrainedBox(
+                        constraints: BoxConstraints(
+                          maxWidth: MediaQuery.of(context).size.width * 0.85,
+                        ),
+                        child: Text(
+                          searches[index].keyword.removeUnderscoreWithSpace(),
+                          overflow: TextOverflow.fade,
+                        ),
+                      ),
+                    ),
+                  );
+                },
               ),
-            );
-          },
-        ),
-        error: (error, stackTrace) => const SizedBox.shrink(),
-        loading: () => const TagChipsPlaceholder(),
-      ),
+            )
+          : const SizedBox.shrink(),
+      error: (error, stackTrace) => const SizedBox.shrink(),
+      loading: () => const TagChipsPlaceholder(),
     );
   }
 }
