c4433d7663cf77f8a5340154914ab9e699c0548e
khoa.nguyen <nguyenduckhoa1996@gmail.com>
2023-06-01 23:54:59 +0700

    fix: gif doesn't work

--- lib/core/domain/video.dart
+++ lib/core/domain/video.dart
@@ -2,5 +2,6 @@ mixin VideoInfoMixin {
   String get format;
   bool get isFlash => format == 'swf';
   bool get isWebm => format == 'webm';
+  bool get isGif => format == 'gif';
   double get duration;
 }
