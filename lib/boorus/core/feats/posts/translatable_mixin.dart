a96e6e9cbc40e41124b83dbf09f5c0ef3495ddc2
khoa.nguyen <nguyenduckhoa1996@gmail.com>
2023-06-05 08:38:06 +0700

    feat: check_translation will count as translated

--- lib/boorus/core/feats/posts/translatable_mixin.dart
+++ lib/boorus/core/feats/posts/translatable_mixin.dart
@@ -1,5 +1,6 @@
 mixin TranslatedMixin {
   List<String> get tags;
 
-  bool get isTranslated => tags.contains('translated');
+  bool get isTranslated =>
+      tags.contains('translated') || tags.contains('check_translation');
 }
