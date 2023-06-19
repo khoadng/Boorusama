9da0ea48d6d905b7e72f5e4d66db6b2572be167f
khoa.nguyen <nguyenduckhoa1996@gmail.com>
2023-06-19 23:48:24 +0700

    feat: add download

--- lib/boorus/e621/e621_provider.dart
+++ lib/boorus/e621/e621_provider.dart
@@ -7,6 +7,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 // Project imports:
 import 'package:boorusama/api/e621/e621_api.dart';
 import 'package:boorusama/boorus/core/feats/boorus/boorus.dart';
+import 'package:boorusama/boorus/core/feats/downloads/downloads.dart';
+import 'package:boorusama/boorus/core/feats/posts/posts.dart';
 import 'package:boorusama/boorus/core/provider.dart';
 import 'package:boorusama/boorus/e621/feats/autocomplete/e621_autocomplete_provider.dart';
 import 'package:boorusama/boorus/e621/feats/posts/e621_post_provider.dart';
@@ -27,8 +29,8 @@ class E621Provider extends StatelessWidget {
         // bulkDownloadFileNameProvider
         //     .overrideWithValue(Md5OnlyFileNameGenerator()),
         postRepoProvider.overrideWith((ref) => ref.watch(e621PostRepoProvider)),
-        // downloadFileNameGeneratorProvider.overrideWith(
-        //     (ref) => ref.watch(moebooruDownloadFileNameGeneratorProvider)),
+        downloadFileNameGeneratorProvider.overrideWith(
+            (ref) => ref.watch(e621DownloadFileNameGeneratorProvider)),
         autocompleteRepoProvider
             .overrideWith((ref) => ref.watch(e621AutocompleteRepoProvider))
       ],
@@ -43,4 +45,9 @@ final e621ApiProvider = Provider<E621Api>((ref) {
   final booruConfig = ref.read(currentBooruConfigProvider);
   final dio = ref.read(dioProvider(booruConfig.url));
   return E621Api(dio);
+});
+
+final e621DownloadFileNameGeneratorProvider =
+    Provider<FileNameGenerator<Post>>((ref) {
+  return Md5OnlyFileNameGenerator();
 });
