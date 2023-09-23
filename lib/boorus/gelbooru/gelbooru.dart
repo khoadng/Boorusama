7cf2d616ffc20ef04ff6a66bf6ba973186ad8f82
khoa.nguyen <nguyenduckhoa1996@gmail.com>
2023-09-23 20:43:45 +0700

    update

--- lib/boorus/gelbooru/gelbooru.dart
+++ lib/boorus/gelbooru/gelbooru.dart
@@ -1,8 +1,8 @@
 // Project imports:
 import 'package:boorusama/boorus/booru_builder.dart';
+import 'package:boorusama/boorus/core/feats/autocompletes/autocompletes.dart';
 import 'package:boorusama/boorus/core/feats/boorus/boorus.dart';
 import 'package:boorusama/boorus/gelbooru/create_gelbooru_config_page.dart';
-import 'package:boorusama/boorus/gelbooru/feats/autocomplete/autocomplete.dart';
 import 'package:boorusama/boorus/gelbooru/feats/posts/posts.dart';
 import 'package:boorusama/boorus/gelbooru/gelbooru_scope.dart';
 
@@ -13,7 +13,7 @@ class GelbooruBuilder implements BooruBuilder {
   });
 
   final GelbooruPostRepositoryApi postRepo;
-  final GelbooruAutocompleteRepositoryApi autocompleteRepo;
+  final AutocompleteRepository autocompleteRepo;
 
   @override
   CreateConfigPageBuilder get createConfigPageBuilder => (
