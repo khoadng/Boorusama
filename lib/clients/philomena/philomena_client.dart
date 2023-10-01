7561ddb8854c82e15827e5ab7a93a1c2b9b5f59f
khoa.nguyen <nguyenduckhoa1996@gmail.com>
2023-10-01 15:18:06 +0700

    add client

---
+++ lib/clients/philomena/philomena_client.dart
@@ -0,0 +1,58 @@
+import 'package:dio/dio.dart';
+
+import 'types/types.dart';
+
+class PhilomenaClient {
+  PhilomenaClient({
+    Dio? dio,
+    required String baseUrl,
+    this.apiKey,
+  }) : _dio = dio ??
+            Dio(BaseOptions(
+              baseUrl: baseUrl,
+            ));
+
+  final Dio _dio;
+  final String? apiKey;
+
+  Future<List<ImageDto>> getImages({
+    List<String>? tags,
+    int? page,
+    int? perPage,
+  }) async {
+    final response = await _dio.get(
+      '/api/v1/json/search/images',
+      queryParameters: {
+        if (tags != null)
+          'q': tags.map((e) => e.replaceAll('_', '+')).join(','),
+        if (page != null) 'page': page,
+        if (perPage != null) 'per_page': perPage,
+        if (apiKey != null) 'key': apiKey,
+      },
+    );
+
+    final data = response.data['images'];
+
+    return (data as List).map((e) => ImageDto.fromJson(e)).toList();
+  }
+
+  Future<List<TagDto>> getTags({
+    required String query,
+    int? page,
+    int? perPage,
+  }) async {
+    final response = await _dio.get(
+      '/api/v1/json/search/tags',
+      queryParameters: {
+        'q': query,
+        if (page != null) 'page': page,
+        if (perPage != null) 'per_page': perPage,
+        if (apiKey != null) 'key': apiKey,
+      },
+    );
+
+    final data = response.data['tags'];
+
+    return (data as List).map((e) => TagDto.fromJson(e)).toList();
+  }
+}
---
+++ lib/clients/philomena/types/tag_dto.dart
@@ -0,0 +1,63 @@
+class TagDto {
+  final dynamic aliasedTag;
+  final List<String>? aliases;
+  final dynamic category;
+  final String? description;
+  // final List<dynamic> dnpEntries;
+  final int? id;
+  final int? images;
+  final List<String>? impliedByTags;
+  final List<String>? impliedTags;
+  final String? name;
+  final String? nameInNamespace;
+  // final dynamic namespace;
+  final String? shortDescription;
+  final String? slug;
+  // final dynamic spoilerImageUri;
+
+  TagDto({
+    this.aliasedTag,
+    this.aliases,
+    this.category,
+    this.description,
+    // this.dnpEntries = const [],
+    this.id,
+    this.images,
+    this.impliedByTags,
+    this.impliedTags,
+    this.name,
+    this.nameInNamespace,
+    // this.namespace,
+    this.shortDescription,
+    this.slug,
+    // this.spoilerImageUri,
+  });
+
+  factory TagDto.fromJson(Map<String, dynamic> json) {
+    return TagDto(
+      aliasedTag: json['aliased_tag'],
+      aliases:
+          (json['aliases'] as List<dynamic>?)?.map((e) => e as String).toList(),
+      category: json['category'],
+      description: json['description'],
+      // dnpEntries: json['dnp_entries'] ?? [],
+      id: json['id'],
+      images: json['images'],
+      impliedByTags: (json['implied_by_tags'] as List<dynamic>?)
+          ?.map((e) => e as String)
+          .toList(),
+      impliedTags: (json['implied_tags'] as List<dynamic>?)
+          ?.map((e) => e as String)
+          .toList(),
+      name: json['name'],
+      nameInNamespace: json['name_in_namespace'],
+      // namespace: json['namespace'],
+      shortDescription: json['short_description'],
+      slug: json['slug'],
+      // spoilerImageUri: json['spoiler_image_uri'],
+    );
+  }
+
+  @override
+  String toString() => '$name';
+}
