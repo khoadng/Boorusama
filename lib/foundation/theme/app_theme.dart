f95b271bcca8b702e9def0656d04396930867c25
khoa.nguyen <nguyenduckhoa1996@gmail.com>
2023-11-18 12:59:25 +0700

    update

--- lib/foundation/theme/app_theme.dart
+++ lib/foundation/theme/app_theme.dart
@@ -5,45 +5,6 @@ import 'package:flutter/material.dart' hide ThemeMode;
 import 'colors.dart';
 import 'theme_mode.dart';
 
-const kLightWhiteColor = Color.fromARGB(255, 220, 220, 220);
-const kDimWhiteColor = Color.fromARGB(255, 130, 130, 130);
-
-// AMOLED Dark theme
-const kPrimaryAmoledDarkColor = Color.fromARGB(255, 86, 99, 233);
-const kOnPrimaryAmoledDarkColor = kLightWhiteColor;
-const kBackgroundAmoledDarkColor = Colors.black;
-const kOnBackgroundAmoledDarkColor = kLightWhiteColor;
-const kSecondaryContainerAmoledDarkColor = Color.fromARGB(255, 50, 50, 50);
-const kSurfaceAmoledDarkColor = Color.fromARGB(255, 40, 40, 40);
-const kOnSurfaceAmoledDarkColor = kLightWhiteColor;
-const kErrorAmoledDarkColor = Colors.redAccent;
-const kOnErrorAmoledDarkColor = kLightWhiteColor;
-const kIconAmoledDarkColor = kDimWhiteColor;
-
-// Dark theme
-const kPrimaryDarkColor = Color.fromARGB(255, 86, 99, 233);
-const kOnPrimaryDarkColor = kLightWhiteColor;
-const kBackgroundDarkColor = Color.fromARGB(255, 32, 32, 32);
-const kOnBackgroundDarkColor = kLightWhiteColor;
-const kSecondaryContainerDarkColor = Color.fromARGB(255, 65, 65, 65);
-const kSurfaceDarkColor = Color.fromARGB(255, 50, 50, 50);
-const kOnSurfaceDarkColor = kLightWhiteColor;
-const kErrorDarkColor = Colors.redAccent;
-const kOnErrorDarkColor = kLightWhiteColor;
-const kIconDarkColor = kLightWhiteColor;
-
-// Light theme
-const kPrimaryLightColor = Color.fromARGB(255, 114, 137, 218);
-const kOnPrimaryLightColor = Colors.white;
-const kBackgroundLightColor = Color.fromARGB(255, 242, 242, 242);
-const kOnBackgroundLightColor = Colors.black;
-const kSecondaryContainerLightColor = Color.fromARGB(255, 225, 227, 229);
-const kSurfaceLightColor = Colors.white;
-const kOnSurfaceLightColor = Colors.black;
-const kErrorLightColor = Colors.redAccent;
-const kOnErrorLightColor = Colors.white;
-const kIconLightColor = Color.fromARGB(255, 79, 86, 96);
-
 class AppTheme {
   AppTheme._();
 
