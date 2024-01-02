98ec304b14d3e0f5e87ef1d8975a814be140d514
khoaharp <nguyenduckhoa1996@gmail.com>
2024-01-02 16:00:16 +0700

    feat: adjust UI for desktop

--- lib/core/widgets/post_tag_list.dart
+++ lib/core/widgets/post_tag_list.dart
@@ -89,10 +89,6 @@ class PostTagListChip extends ConsumerWidget {
       ref.getTagColor(context, tag.category.name),
       ref.watch(settingsProvider),
     );
-    final numberColors = context.generateChipColors(
-      Colors.grey[600]!,
-      ref.watch(settingsProvider),
-    );
 
     return Row(
       mainAxisSize: MainAxisSize.min,
@@ -100,6 +96,7 @@ class PostTagListChip extends ConsumerWidget {
         SizedBox(
           height: 28,
           child: Chip(
+            padding: EdgeInsets.zero,
             visualDensity: const ShrinkVisualDensity(),
             backgroundColor: colors?.backgroundColor,
             side: colors != null
@@ -108,56 +105,38 @@ class PostTagListChip extends ConsumerWidget {
                     width: 1,
                   )
                 : null,
-            shape: RoundedRectangleBorder(
-              borderRadius: !ref.watchConfig.hasStrictSFW
-                  ? const BorderRadius.only(
-                      topLeft: Radius.circular(8),
-                      bottomLeft: Radius.circular(8),
-                    )
-                  : const BorderRadius.all(Radius.circular(8)),
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(8)),
             ),
             label: ConstrainedBox(
               constraints: BoxConstraints(
                 maxWidth: maxTagWidth ?? context.screenWidth * 0.7,
               ),
-              child: Text(
-                _getTagStringDisplayName(tag),
-                overflow: TextOverflow.fade,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: colors?.foregroundColor,
+              child: RichText(
+                overflow: TextOverflow.ellipsis,
+                text: TextSpan(
+                  text: _getTagStringDisplayName(tag),
+                  style: TextStyle(
+                    color: colors?.foregroundColor,
+                  ),
+                  children: [
+                    if (!ref.watchConfig.hasStrictSFW)
+                      TextSpan(
+                        text:
+                            '  ${NumberFormat.compact().format(tag.postCount)}',
+                        style: context.textTheme.bodySmall?.copyWith(
+                          fontSize: 11,
+                          color: context.themeMode.isLight
+                              ? Colors.white.withOpacity(0.85)
+                              : Colors.grey.withOpacity(0.85),
+                        ),
+                      ),
+                  ],
                 ),
               ),
             ),
           ),
         ),
-        if (!ref.watchConfig.hasStrictSFW)
-          SizedBox(
-            height: 28,
-            child: Chip(
-              visualDensity: const ShrinkVisualDensity(),
-              backgroundColor: numberColors?.backgroundColor,
-              side: numberColors != null
-                  ? BorderSide(
-                      color: numberColors.borderColor,
-                      width: 1,
-                    )
-                  : null,
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.only(
-                  topRight: Radius.circular(8),
-                  bottomRight: Radius.circular(8),
-                ),
-              ),
-              label: Text(
-                NumberFormat.compact().format(tag.postCount),
-                style: TextStyle(
-                  color: numberColors?.foregroundColor,
-                  fontSize: 12,
-                ),
-              ),
-            ),
-          ),
       ],
     );
   }
