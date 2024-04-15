497253939e38af068a19e9f5d119216b2db68332
khoa.nguyen <nguyenduckhoa1996@gmail.com>
2024-04-15 12:55:26 +0700

    feat: update UI for pool sort

--- lib/boorus/danbooru/pages/widgets/pools/pool_options_header.dart
+++ lib/boorus/danbooru/pages/widgets/pools/pool_options_header.dart
@@ -3,10 +3,12 @@ import 'package:flutter/material.dart';
 
 // Package imports:
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:material_symbols_icons/symbols.dart';
 
 // Project imports:
 import 'package:boorusama/boorus/danbooru/feats/pools/pools.dart';
 import 'package:boorusama/foundation/i18n.dart';
+import 'package:boorusama/foundation/theme/theme.dart';
 import 'package:boorusama/widgets/widgets.dart';
 
 class PoolOptionsHeader extends ConsumerWidget {
@@ -18,30 +20,23 @@ class PoolOptionsHeader extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final order = ref.watch(danbooruSelectedPoolOrderProvider);
 
-    return Padding(
+    return Container(
+      color: context.colorScheme.surface,
       padding: const EdgeInsets.symmetric(
-        horizontal: 8,
         vertical: 12,
       ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          OptionDropDownButton(
-            value: order,
-            alignment: AlignmentDirectional.centerStart,
-            onChanged: (value) {
-              if (value == null) return;
-              ref.read(danbooruSelectedPoolOrderProvider.notifier).state =
-                  value;
-            },
-            items: PoolOrder.values
-                .map((e) => DropdownMenuItem(
-                      value: e,
-                      child: Text(_poolOrderToString(e)).tr(),
-                    ))
-                .toList(),
-          ),
-        ],
+      child: ChoiceOptionSelectorList(
+        icon: const Icon(Symbols.sort),
+        options: PoolOrder.values,
+        hasNullOption: false,
+        optionLabelBuilder: (value) =>
+            value != null ? _poolOrderToString(value).tr() : 'All',
+        sheetTitle: 'Sort by',
+        onSelected: (value) {
+          ref.read(danbooruSelectedPoolOrderProvider.notifier).state =
+              value ?? PoolOrder.newest;
+        },
+        selectedOption: order,
       ),
     );
   }
@@ -51,5 +46,5 @@ String _poolOrderToString(PoolOrder order) => switch (order) {
       PoolOrder.newest => 'pool.order.new',
       PoolOrder.postCount => 'pool.order.post_count',
       PoolOrder.name => 'pool.order.name',
-      PoolOrder.latest => 'pool.order.recent'
+      PoolOrder.latest => 'pool.order.recent',
     };
