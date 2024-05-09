3b292e43d6ae6906cad8f61d74013e6902d82e2a
khoa.nguyen <nguyenduckhoa1996@gmail.com>
2024-05-10 00:33:39 +0700

    update

--- lib/boorus/gelbooru_v1/create_gelbooru_v1_config_page.dart
+++ lib/boorus/gelbooru_v1/create_gelbooru_v1_config_page.dart
@@ -5,11 +5,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 // Project imports:
+import 'package:boorusama/core/configs/configs.dart';
 import 'package:boorusama/core/feats/boorus/boorus.dart';
-import 'package:boorusama/core/scaffolds/scaffolds.dart';
-import 'package:boorusama/router.dart';
 
-class CreateGelbooruV1ConfigPage extends ConsumerStatefulWidget {
+class CreateGelbooruV1ConfigPage extends StatelessWidget {
   const CreateGelbooruV1ConfigPage({
     super.key,
     required this.config,
@@ -22,46 +21,17 @@ class CreateGelbooruV1ConfigPage extends ConsumerStatefulWidget {
   final bool isNewConfig;
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() =>
-      _CreateGelbooruV1ConfigPageState();
-}
-
-class _CreateGelbooruV1ConfigPageState
-    extends ConsumerState<CreateGelbooruV1ConfigPage> {
-  @override
   Widget build(BuildContext context) {
-    return CreateBooruConfigScaffold(
-      isNewConfig: widget.isNewConfig,
-      backgroundColor: widget.backgroundColor,
-      config: widget.config,
-      hasDownloadTab: true,
-      tabsBuilder: (context) => {},
-      submit: submit,
+    return ProviderScope(
+      overrides: [
+        initialBooruConfigProvider.overrideWithValue(config),
+      ],
+      child: CreateBooruConfigScaffold(
+        isNewConfig: isNewConfig,
+        backgroundColor: backgroundColor,
+        hasDownloadTab: true,
+        tabsBuilder: (context) => {},
+      ),
     );
-  }
-
-  void submit(CreateConfigData data) {
-    final config = AddNewBooruConfig(
-      login: '',
-      apiKey: '',
-      booru: widget.config.booruType,
-      booruHint: widget.config.booruType,
-      configName: data.configName,
-      hideDeleted: false,
-      ratingFilter: BooruConfigRatingFilter.none,
-      url: widget.config.url,
-      customDownloadFileNameFormat: data.customDownloadFileNameFormat,
-      customBulkDownloadFileNameFormat: data.customBulkDownloadFileNameFormat,
-      imageDetaisQuality: data.imageDetaisQuality,
-      granularRatingFilters: null,
-      postGestures: data.postGestures,
-      defaultPreviewImageButtonAction: data.defaultPreviewImageButtonAction,
-    );
-
-    ref
-        .read(booruConfigProvider.notifier)
-        .addOrUpdate(config: widget.config, newConfig: config);
-
-    context.pop();
   }
 }
