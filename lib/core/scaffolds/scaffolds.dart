0ed08e209b773c35ca55e7e1e2bf294d2a049a39
khoa.nguyen <nguyenduckhoa1996@gmail.com>
2024-05-11 01:59:15 +0700

    delet legacy

--- lib/core/scaffolds/scaffolds.dart
+++ lib/core/scaffolds/scaffolds.dart
@@ -1,6 +1,5 @@
 export 'artist_page_scaffold.dart';
 export 'comment_page_scaffold.dart';
-export 'create_booru_config_scaffold.dart';
 export 'desktop_home_page_scaffold.dart';
 export 'favorite_page_scaffold.dart';
 export 'home_page_scaffold.dart';
