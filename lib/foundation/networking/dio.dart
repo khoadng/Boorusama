068eacd16bf97829737f650be974d9ebb9fcc4da
khoa.nguyen <nguyenduckhoa1996@gmail.com>
2024-06-07 15:02:57 +0700

    fix: remove null ua

--- lib/foundation/networking/dio.dart
+++ lib/foundation/networking/dio.dart
@@ -92,7 +92,6 @@ class AppHttpOverrides extends HttpOverrides {
   @override
   HttpClient createHttpClient(SecurityContext? context) {
     return super.createHttpClient(context)
-      ..userAgent = null
       ..idleTimeout = const Duration(seconds: 30);
   }
 }
