b66b71a771126d6681fceee95e645a19ecdf97f0
khoa.nguyen <nguyenduckhoa1996@gmail.com>
2024-06-17 16:24:16 +0700

    fix: dynamic theme issue with new m3 roles

--- lib/foundation/theme/theme_buider.dart
+++ lib/foundation/theme/theme_buider.dart
@@ -30,7 +30,13 @@ class ThemeBuilder extends ConsumerWidget {
         MediaQuery.platformBrightnessOf(context) == Brightness.dark;
 
     return DynamicColorBuilder(
-      builder: (light, dark) {
+      builder: (lightOrigin, darkOrigin) {
+        final (light, dark) = enableDynamicColor
+            ? lightOrigin != null && darkOrigin != null
+                ? _generateDynamicColourSchemes(lightOrigin, darkOrigin)
+                : (null, null)
+            : (null, null);
+
         final darkScheme = AppTheme.generateFromThemeMode(
           theme,
           seed: enableDynamicColor ? dark : null,
@@ -74,3 +80,44 @@ class ThemeBuilder extends ConsumerWidget {
     );
   }
 }
+
+//TODO: temp solution
+// https://github.com/material-foundation/flutter-packages/issues/582
+(ColorScheme light, ColorScheme dark) _generateDynamicColourSchemes(
+    ColorScheme lightDynamic, ColorScheme darkDynamic) {
+  var lightBase = ColorScheme.fromSeed(seedColor: lightDynamic.primary);
+  var darkBase = ColorScheme.fromSeed(
+      seedColor: darkDynamic.primary, brightness: Brightness.dark);
+
+  var lightAdditionalColours = _extractAdditionalColours(lightBase);
+  var darkAdditionalColours = _extractAdditionalColours(darkBase);
+
+  var lightScheme = _insertAdditionalColours(lightBase, lightAdditionalColours);
+  var darkScheme = _insertAdditionalColours(darkBase, darkAdditionalColours);
+
+  return (lightScheme.harmonized(), darkScheme.harmonized());
+}
+
+List<Color> _extractAdditionalColours(ColorScheme scheme) => [
+      scheme.surface,
+      scheme.surfaceDim,
+      scheme.surfaceBright,
+      scheme.surfaceContainerLowest,
+      scheme.surfaceContainerLow,
+      scheme.surfaceContainer,
+      scheme.surfaceContainerHigh,
+      scheme.surfaceContainerHighest,
+    ];
+
+ColorScheme _insertAdditionalColours(
+        ColorScheme scheme, List<Color> additionalColours) =>
+    scheme.copyWith(
+      surface: additionalColours[0],
+      surfaceDim: additionalColours[1],
+      surfaceBright: additionalColours[2],
+      surfaceContainerLowest: additionalColours[3],
+      surfaceContainerLow: additionalColours[4],
+      surfaceContainer: additionalColours[5],
+      surfaceContainerHigh: additionalColours[6],
+      surfaceContainerHighest: additionalColours[7],
+    );
