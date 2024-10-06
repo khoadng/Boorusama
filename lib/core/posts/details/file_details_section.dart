4a919ee7ce02a2da8e3a635930e7c50ac74fc3ff
khoaharp <nguyenduckhoa1996@gmail.com>
2024-10-06 19:36:20 +0700

    feat: add `initialExpanded`

--- lib/core/posts/details/file_details_section.dart
+++ lib/core/posts/details/file_details_section.dart
@@ -18,9 +18,11 @@ class DefaultFileDetailsSection extends StatelessWidget {
     required this.post,
     this.uploaderName,
     this.customDetails,
+    this.initialExpanded = false,
   });
 
   final Post post;
+  final bool initialExpanded;
   final String? uploaderName;
   final Map<String, Widget>? customDetails;
 
@@ -50,12 +52,14 @@ class FileDetailsSection extends StatelessWidget {
     required this.rating,
     this.uploader,
     this.customDetails,
+    this.initialExpanded = false,
   });
 
   final Post post;
   final Rating rating;
   final Widget? uploader;
   final Map<String, Widget>? customDetails;
+  final bool initialExpanded;
 
   @override
   Widget build(BuildContext context) {
@@ -82,6 +86,7 @@ class FileDetailsSection extends StatelessWidget {
         dividerColor: Colors.transparent,
       ),
       child: ExpansionTile(
+        initiallyExpanded: initialExpanded,
         title: Text(
           'post.detail.file_details'.tr(),
         ),
