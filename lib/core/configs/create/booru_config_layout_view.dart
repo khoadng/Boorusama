5c559ad2935ec4903375ea3d5b4945f29d31adb9
khoa.nguyen <nguyenduckhoa1996@gmail.com>
2024-11-27 18:57:58 +0700

    update

--- lib/core/configs/create/booru_config_layout_view.dart
+++ lib/core/configs/create/booru_config_layout_view.dart
@@ -22,26 +22,34 @@ class DefaultBooruConfigLayoutView extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final config = ref.watch(initialBooruConfigProvider);
-    final booruBuilder = ref.watchBooruBuilder(config);
-    final data = booruBuilder?.customHomeViewBuilders ?? kDefaultAltHomeView;
-
-    return BooruConfigLayoutView(
-      altHomeView: data.keys.toList(),
-      decribeView: (viewKey) => data[viewKey]?.displayName ?? 'Unknown',
-    );
+    return const BooruConfigLayoutView();
   }
 }
 
 class BooruConfigLayoutView extends ConsumerWidget {
   const BooruConfigLayoutView({
     super.key,
-    required this.altHomeView,
-    required this.decribeView,
   });
 
-  final List<CustomHomeViewKey> altHomeView;
-  final String Function(CustomHomeViewKey viewKey) decribeView;
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return SingleChildScrollView(
+      padding: const EdgeInsets.symmetric(horizontal: 12),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const _HomeScreenSection(),
+          const Divider(),
+          const _CustomDetailsSection(),
+        ],
+      ),
+    );
+  }
+}
+
+class _CustomDetailsSection extends ConsumerWidget {
+  const _CustomDetailsSection();
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -52,158 +60,151 @@ class BooruConfigLayoutView extends ConsumerWidget {
         LayoutConfigs.undefined();
 
     final uiBuilder = ref.watchBooruBuilder(config)?.postDetailsUIBuilder;
-    final details = layout.details;
+    final details = layout.details ??
+        convertDetailsParts(uiBuilder?.full.keys.toList() ?? []);
 
-    return SingleChildScrollView(
-      padding: const EdgeInsets.symmetric(horizontal: 12),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ListTile(
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8),
+          child: ListTile(
             contentPadding: EdgeInsets.zero,
             visualDensity: VisualDensity.compact,
-            title: const Text("Home screen"),
-            subtitle: const Text(
-              'Change the view of the home screen',
-            ),
-            trailing: OptionDropDownButton(
-              alignment: AlignmentDirectional.centerStart,
-              value: layout.home,
-              onChanged: (value) => ref.editNotifier.updateLayout(
-                layout.copyWith(home: () => value),
-              ),
-              items: altHomeView
-                  .map((value) => DropdownMenuItem(
-                        value: value,
-                        child: Text(decribeView(value)).tr(),
-                      ))
-                  .toList(),
-            ),
-          ),
-          Divider(),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8),
-            child: ListTile(
-              contentPadding: EdgeInsets.zero,
-              visualDensity: VisualDensity.compact,
-              title: const Text("Information widgets"),
-              trailing: uiBuilder != null
-                  ? TextButton(
-                      child: const Text('Customize'),
-                      onPressed: () => Navigator.of(context).push(
-                        CupertinoPageRoute(
-                          builder: (context) => CustomDetailsChooserPage(
-                            availableParts: uiBuilder.full.keys.toList(),
-                            selectedParts:
-                                layout.getParsedParts()?.toList() ?? [],
-                            onDone: (parts) => ref.editNotifier.updateLayout(
-                              layout.copyWith(
-                                details: () => convertDetailsParts(parts),
-                              ),
+            title: const Text("Information widgets"),
+            trailing: uiBuilder != null
+                ? TextButton(
+                    child: const Text('Customize'),
+                    onPressed: () => Navigator.of(context).push(
+                      CupertinoPageRoute(
+                        builder: (context) => CustomDetailsChooserPage(
+                          availableParts: uiBuilder.full.keys.toList(),
+                          selectedParts:
+                              layout.getParsedParts()?.toList() ?? [],
+                          onDone: (parts) => ref.editNotifier.updateLayout(
+                            layout.copyWith(
+                              details: () => convertDetailsParts(parts),
                             ),
                           ),
                         ),
                       ),
-                    )
-                  : null,
-            ),
+                    ),
+                  )
+                : null,
           ),
-          if (uiBuilder != null)
-            Container(
-              margin: const EdgeInsets.symmetric(horizontal: 4),
-              child: details != null && details.isNotEmpty
-                  ? Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        ReorderableColumn(
-                          onReorder: (int oldIndex, int newIndex) {
-                            final newDetails = details.toList();
+        ),
+        if (uiBuilder != null)
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 4),
+            child: ReorderableColumn(
+              onReorder: (int oldIndex, int newIndex) {
+                final newDetails = details.toList();
 
-                            final item = newDetails.removeAt(oldIndex);
-                            newDetails.insert(newIndex, item);
+                final item = newDetails.removeAt(oldIndex);
+                newDetails.insert(newIndex, item);
 
-                            ref.editNotifier.updateLayout(
-                              layout.copyWith(details: () => newDetails),
-                            );
-                          },
-                          children: details
-                              .map(
-                                (e) => Container(
-                                  key: ValueKey(e.name),
-                                  padding: const EdgeInsets.symmetric(
-                                    horizontal: 4,
-                                  ),
-                                  margin: const EdgeInsets.symmetric(
-                                    vertical: 4,
-                                  ),
-                                  decoration: BoxDecoration(
-                                    border: Border.all(
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .outlineVariant,
-                                    ),
-                                    color: Theme.of(context)
-                                        .colorScheme
-                                        .surfaceContainerLow,
-                                    borderRadius: BorderRadius.circular(8),
-                                  ),
-                                  child: ListTile(
-                                    leading: Icon(
-                                      Icons.drag_indicator,
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .hintColor,
-                                    ),
-                                    trailing: BooruPopupMenuButton(
-                                      onSelected: (value) {
-                                        if (value == 'remove') {
-                                          if (details.length == 1) {
-                                            showErrorToast(
-                                              context,
-                                              'At least one item is required',
-                                            );
-                                            return;
-                                          }
-
-                                          ref.editNotifier.updateLayout(
-                                            layout.copyWith(
-                                              details: () => details
-                                                  .where(
-                                                      (element) => element != e)
-                                                  .toList(),
-                                            ),
-                                          );
-                                        }
-                                      },
-                                      itemBuilder: {
-                                        'remove': const Text('Remove'),
-                                      },
-                                    ),
-                                    contentPadding: const EdgeInsets.symmetric(
-                                      horizontal: 8,
-                                    ),
-                                    title: Text(e.name),
-                                  ),
-                                ),
-                              )
-                              .toList(),
+                ref.editNotifier.updateLayout(
+                  layout.copyWith(details: () => newDetails),
+                );
+              },
+              children: details
+                  .map(
+                    (e) => Container(
+                      key: ValueKey(e.name),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 4,
+                      ),
+                      margin: const EdgeInsets.symmetric(
+                        vertical: 4,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Theme.of(context).colorScheme.outlineVariant,
                         ),
-                      ],
-                    )
-                  : Container(
-                      color: Theme.of(context).colorScheme.surfaceContainerLow,
+                        color:
+                            Theme.of(context).colorScheme.surfaceContainerLow,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
                       child: ListTile(
+                        leading: Icon(
+                          Icons.drag_indicator,
+                          color: Theme.of(context).colorScheme.hintColor,
+                        ),
+                        trailing: BooruPopupMenuButton(
+                          onSelected: (value) {
+                            if (value == 'remove') {
+                              if (details.length == 1) {
+                                showErrorToast(
+                                  context,
+                                  'At least one item is required',
+                                );
+                                return;
+                              }
+
+                              ref.editNotifier.updateLayout(
+                                layout.copyWith(
+                                  details: () => details
+                                      .where((element) => element != e)
+                                      .toList(),
+                                ),
+                              );
+                            }
+                          },
+                          itemBuilder: {
+                            'remove': const Text('Remove'),
+                          },
+                        ),
                         contentPadding: const EdgeInsets.symmetric(
                           horizontal: 8,
                         ),
-                        visualDensity: VisualDensity.compact,
-                        title: Text('No custom widgets, use default'),
+                        title: Text(e.name),
                       ),
                     ),
+                  )
+                  .toList(),
             ),
-        ],
+          ),
+      ],
+    );
+  }
+}
+
+class _HomeScreenSection extends ConsumerWidget {
+  const _HomeScreenSection();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final config = ref.watch(initialBooruConfigProvider);
+    final booruBuilder = ref.watchBooruBuilder(config);
+    final layout = ref.watch(
+            editBooruConfigProvider(ref.watch(editBooruConfigIdProvider))
+                .select((value) => value.layoutTyped)) ??
+        LayoutConfigs.undefined();
+    final data = booruBuilder?.customHomeViewBuilders ?? kDefaultAltHomeView;
+
+    return ListTile(
+      contentPadding: EdgeInsets.zero,
+      visualDensity: VisualDensity.compact,
+      title: const Text("Home screen"),
+      subtitle: const Text(
+        'Change the view of the home screen',
+      ),
+      trailing: OptionDropDownButton(
+        alignment: AlignmentDirectional.centerStart,
+        value: layout.home,
+        onChanged: (value) => ref.editNotifier.updateLayout(
+          layout.copyWith(home: () => value),
+        ),
+        items: data.keys
+            .map((value) => DropdownMenuItem(
+                  value: value,
+                  child: Text(_describeView(data, value)).tr(),
+                ))
+            .toList(),
       ),
     );
   }
+
+  String _describeView(Map<CustomHomeViewKey, CustomHomeDataBuilder> data,
+          CustomHomeViewKey viewKey) =>
+      data[viewKey]?.displayName ?? 'Unknown';
 }
