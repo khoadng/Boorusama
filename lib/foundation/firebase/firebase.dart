efeee82863c4342d3790756cf3bde221b9435db6
khoa.nguyen <nguyenduckhoa1996@gmail.com>
2024-12-06 02:08:36 +0700

    feat: reorganize Firebase service integration

--- lib/foundation/firebase/firebase.dart
+++ lib/foundation/firebase/firebase.dart
@@ -8,36 +8,60 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:boorusama/core/settings/settings.dart';
 import 'package:boorusama/foundation/analytics.dart';
 import 'package:boorusama/foundation/error.dart';
-import 'package:boorusama/foundation/platform.dart';
+import 'package:boorusama/foundation/firebase/firebase_options_dev.dart' as dev;
+import '../loggers/loggers.dart';
 import 'firebase_analytics.dart';
 import 'firebase_crashlytics.dart';
-import 'firebase_options.dart';
+
+import 'package:boorusama/foundation/firebase/firebase_options_prod.dart'
+    as prod;
 
 export 'firebase_analytics.dart';
 export 'firebase_crashlytics.dart';
 
+const _kEnv = String.fromEnvironment('ENV_NAME', defaultValue: '');
+
 Future<(AnalyticsInterface? analytics, ErrorReporter? reporter)>
-    ensureFirebaseInitialized(Settings settings) async {
-  if (!_isFirebasePlatformSupported()) {
+    ensureFirebaseInitialized(
+  Settings settings, {
+  Logger? logger,
+}) async {
+  final options = _tryGetFirebaseOptions(_kEnv, logger);
+
+  if (options == null) {
+    logger?.logW('Firebase', 'Firebase options are not available, skipping');
     return (null, null);
   }
 
   // Always initialize to prevent crashings
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+    options: options,
   );
 
+  final dataCollectingStatus = settings.dataCollectingStatus;
+  final isEnabled =
+      isFirebaseEnabled(dataCollectingStatus: dataCollectingStatus);
+
   final firebaseAnalytics = FirebaseAnalyticsImpl(
-    dataCollectingStatus: settings.dataCollectingStatus,
+    enabled: isEnabled,
   );
-  final crashlyticsReporter = FirebaseCrashlyticsReporter();
+  final crashlyticsReporter = FirebaseCrashlyticsReporter(
+    isRemoteErrorReportingSupported: isEnabled,
+  );
 
   await firebaseAnalytics.ensureInitialized();
   await crashlyticsReporter.enstureInitialized();
 
-  return isFirebaseEnabled(dataCollectingStatus: settings.dataCollectingStatus)
-      ? (firebaseAnalytics, crashlyticsReporter)
-      : (null, null);
+  if (isEnabled) {
+    logger?.logI('Firebase', 'All Firebase services are initialized');
+    return (firebaseAnalytics, crashlyticsReporter);
+  } else {
+    logger?.logW(
+      'Firebase',
+      'Firebase is disabled: status=$dataCollectingStatus, release=$kReleaseMode',
+    );
+    return (null, null);
+  }
 }
 
 bool isFirebaseEnabled({
@@ -45,4 +69,17 @@ bool isFirebaseEnabled({
 }) =>
     dataCollectingStatus == DataCollectingStatus.allow && kReleaseMode;
 
-bool _isFirebasePlatformSupported() => isAndroid() || isIOS() || isMacOS();
+FirebaseOptions? _tryGetFirebaseOptions(String env, Logger? logger) {
+  try {
+    final options = switch (_kEnv) {
+      'prod' => prod.DefaultFirebaseOptions.currentPlatform,
+      'dev' => dev.DefaultFirebaseOptions.currentPlatform,
+      _ => throw UnsupportedError('Invalid environment: $_kEnv'),
+    };
+
+    return options;
+  } catch (e) {
+    logger?.logE('Firebase', 'Failed to get Firebase options: $e');
+    return null;
+  }
+}
--- lib/foundation/firebase/firebase_crashlytics.dart
+++ lib/foundation/firebase/firebase_crashlytics.dart
@@ -9,38 +9,55 @@ import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 // Project imports:
 import 'package:boorusama/foundation/error.dart';
+import 'package:boorusama/foundation/loggers/loggers.dart';
 import 'package:boorusama/foundation/package_info.dart';
 import 'package:boorusama/foundation/platform.dart';
 
 class FirebaseCrashlyticsReporter implements ErrorReporter {
+  FirebaseCrashlyticsReporter({
+    required this.isRemoteErrorReportingSupported,
+    this.logger,
+  });
+
+  final Logger? logger;
+
   @override
-  bool get isRemoteErrorReportingSupported =>
-      isAndroid() || isIOS() || isMacOS();
+  final bool isRemoteErrorReportingSupported;
+
+  bool isPlatformSupported() => isAndroid() || isIOS() || isMacOS();
 
   @override
   void recordFlutterFatalError(FlutterErrorDetails details) {
-    FirebaseCrashlytics.instance.recordFlutterFatalError(details);
+    if (isRemoteErrorReportingSupported) {
+      FirebaseCrashlytics.instance.recordFlutterFatalError(details);
+    }
   }
 
   @override
   void recordError(error, stackTrace) {
-    FirebaseCrashlytics.instance.recordError(error, stackTrace, fatal: true);
+    if (isRemoteErrorReportingSupported) {
+      FirebaseCrashlytics.instance.recordError(error, stackTrace, fatal: true);
+    }
   }
 
   Future<void> enstureInitialized() async {
-    await FirebaseCrashlytics.instance
-        .setCrashlyticsCollectionEnabled(!kDebugMode);
+    if (isPlatformSupported()) {
+      await FirebaseCrashlytics.instance
+          .setCrashlyticsCollectionEnabled(isRemoteErrorReportingSupported);
 
-    await FirebaseCrashlytics.instance
-        .setCustomKey('locale', Platform.localeName);
+      await FirebaseCrashlytics.instance
+          .setCustomKey('locale', Platform.localeName);
 
-    await FirebaseCrashlytics.instance
-        .setCustomKey('time-zone-name', DateTime.now().timeZoneName);
+      await FirebaseCrashlytics.instance
+          .setCustomKey('time-zone-name', DateTime.now().timeZoneName);
 
-    await FirebaseCrashlytics.instance.setCustomKey(
-        'time-zone-offset', DateTime.now().timeZoneOffset.inHours);
+      await FirebaseCrashlytics.instance.setCustomKey(
+          'time-zone-offset', DateTime.now().timeZoneOffset.inHours);
 
-    await FirebaseCrashlytics.instance
-        .setCustomKey('environment', kEnvironment);
+      await FirebaseCrashlytics.instance
+          .setCustomKey('environment', kEnvironment);
+    } else {
+      logger?.logE('FirebaseCrashlytics', 'Platform is not supported');
+    }
   }
 }
