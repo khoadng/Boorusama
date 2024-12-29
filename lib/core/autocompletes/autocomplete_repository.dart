95671035622c32a33d280686d274de3e68e028d9
khoa.nguyen <nguyenduckhoa1996@gmail.com>
2024-12-29 22:54:49 +0700

    fix: remove more caching

--- lib/core/autocompletes/autocomplete_repository.dart
+++ lib/core/autocompletes/autocomplete_repository.dart
@@ -1,6 +1,3 @@
-// Dart imports:
-import 'dart:convert';
-
 // Flutter imports:
 import 'package:flutter/foundation.dart';
 
@@ -9,7 +6,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 // Project imports:
 import 'package:boorusama/core/autocompletes/autocomplete.dart';
-import 'package:boorusama/foundation/caching/caching.dart';
 import 'package:boorusama/foundation/debugs/print.dart';
 
 abstract class AutocompleteRepository {
@@ -17,10 +13,10 @@ abstract class AutocompleteRepository {
 }
 
 class AutocompleteRepositoryBuilder
-    with PersistentCacheMixin, DebugPrintMixin
+    with DebugPrintMixin
     implements AutocompleteRepository {
   AutocompleteRepositoryBuilder({
-    this.persistentStaleDuration = const Duration(days: 3),
+    this.persistentStaleDuration = const Duration(days: 1),
     required this.autocomplete,
     required this.persistentStorageKey,
   });
@@ -34,32 +30,13 @@ class AutocompleteRepositoryBuilder
       return Future.value([]);
     }
 
-    final cached = await load(query);
-
-    if (cached != null) {
-      final json = jsonDecode(cached) as List<dynamic>;
-
-      final data = json.map((e) => AutocompleteData.fromJson(e)).toList();
-
-      printDebug('Loaded from cache for query $query');
-      return data;
-    }
-
     final fresh = await autocomplete(query);
-
-    final json = fresh.map((e) => e.toJson()).toList();
-
-    await save(query, jsonEncode(json));
-
-    printDebug('Loaded from network for query $query');
 
     return fresh;
   }
 
-  @override
+  //TODO: remove this or maybe have a better caching strategy without using Hive
   final Duration persistentStaleDuration;
-
-  @override
   final String persistentStorageKey;
 
   @override
