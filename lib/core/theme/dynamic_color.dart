26d458bd37124a22f522e428f1a35c4af1184dad
khoa.nguyen <nguyenduckhoa1996@gmail.com>
2025-01-18 15:53:10 +0700

    refactor: add a separated widget to wrap around dynamic color package

---
+++ lib/core/theme/dynamic_color.dart
@@ -0,0 +1,86 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+// Package imports:
+import 'package:dynamic_color/dynamic_color.dart';
+
+class AppDynamicColorBuilder extends StatelessWidget {
+  const AppDynamicColorBuilder({
+    required this.builder,
+    super.key,
+  });
+
+  final Widget Function(
+    ColorScheme? lightDynamic,
+    ColorScheme? darkDynamic,
+  ) builder;
+
+  @override
+  Widget build(BuildContext context) {
+    return DynamicColorBuilder(
+      builder: (lightOrigin, darkOrigin) {
+        final (light, dark) = lightOrigin != null && darkOrigin != null
+            ? _generateDynamicColourSchemes(lightOrigin, darkOrigin)
+            : (null, null);
+
+        return builder(light, dark);
+      },
+    );
+  }
+}
+
+//TODO: temp solution
+// https://github.com/material-foundation/flutter-packages/issues/582
+(ColorScheme light, ColorScheme dark) _generateDynamicColourSchemes(
+  ColorScheme lightDynamic,
+  ColorScheme darkDynamic,
+) {
+  final lightBase = ColorScheme.fromSeed(seedColor: lightDynamic.primary);
+  final darkBase = ColorScheme.fromSeed(
+    seedColor: darkDynamic.primary,
+    brightness: Brightness.dark,
+  );
+
+  final lightAdditionalColours = _extractAdditionalColours(lightBase);
+  final darkAdditionalColours = _extractAdditionalColours(darkBase);
+
+  final lightScheme = _insertAdditionalColours(
+    lightBase,
+    lightAdditionalColours,
+  );
+  final darkScheme = _insertAdditionalColours(
+    darkBase,
+    darkAdditionalColours,
+  );
+
+  return (
+    lightScheme.harmonized(),
+    darkScheme.harmonized(),
+  );
+}
+
+List<Color> _extractAdditionalColours(ColorScheme scheme) => [
+      scheme.surface,
+      scheme.surfaceDim,
+      scheme.surfaceBright,
+      scheme.surfaceContainerLowest,
+      scheme.surfaceContainerLow,
+      scheme.surfaceContainer,
+      scheme.surfaceContainerHigh,
+      scheme.surfaceContainerHighest,
+    ];
+
+ColorScheme _insertAdditionalColours(
+  ColorScheme scheme,
+  List<Color> additionalColours,
+) =>
+    scheme.copyWith(
+      surface: additionalColours[0],
+      surfaceDim: additionalColours[1],
+      surfaceBright: additionalColours[2],
+      surfaceContainerLowest: additionalColours[3],
+      surfaceContainerLow: additionalColours[4],
+      surfaceContainer: additionalColours[5],
+      surfaceContainerHigh: additionalColours[6],
+      surfaceContainerHighest: additionalColours[7],
+    );
--- lib/core/theme/theme_buider.dart
+++ lib/core/theme/theme_buider.dart
@@ -2,7 +2,6 @@
 import 'package:flutter/material.dart';
 
 // Package imports:
-import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 // Project imports:
@@ -12,6 +11,7 @@ import '../premiums/premiums.dart';
 import '../settings/providers.dart';
 import 'app_theme.dart';
 import 'colors.dart';
+import 'dynamic_color.dart';
 import 'theme_configs.dart';
 import 'theme_mode.dart';
 
@@ -44,13 +44,10 @@ class ThemeBuilder extends ConsumerWidget {
     final systemDarkMode =
         MediaQuery.platformBrightnessOf(context) == Brightness.dark;
 
-    return DynamicColorBuilder(
+    return AppDynamicColorBuilder(
       builder: (lightOrigin, darkOrigin) {
-        final (light, dark) = enableDynamicColor
-            ? lightOrigin != null && darkOrigin != null
-                ? _generateDynamicColourSchemes(lightOrigin, darkOrigin)
-                : (null, null)
-            : (null, null);
+        final (light, dark) =
+            enableDynamicColor ? (lightOrigin, darkOrigin) : (null, null);
 
         final customColorScheme = hasPremium
             ? ref.watchThemeConfigs?.enable == true
@@ -97,59 +94,3 @@ class ThemeBuilder extends ConsumerWidget {
     );
   }
 }
-
-//TODO: temp solution
-// https://github.com/material-foundation/flutter-packages/issues/582
-(ColorScheme light, ColorScheme dark) _generateDynamicColourSchemes(
-  ColorScheme lightDynamic,
-  ColorScheme darkDynamic,
-) {
-  final lightBase = ColorScheme.fromSeed(seedColor: lightDynamic.primary);
-  final darkBase = ColorScheme.fromSeed(
-    seedColor: darkDynamic.primary,
-    brightness: Brightness.dark,
-  );
-
-  final lightAdditionalColours = _extractAdditionalColours(lightBase);
-  final darkAdditionalColours = _extractAdditionalColours(darkBase);
-
-  final lightScheme = _insertAdditionalColours(
-    lightBase,
-    lightAdditionalColours,
-  );
-  final darkScheme = _insertAdditionalColours(
-    darkBase,
-    darkAdditionalColours,
-  );
-
-  return (
-    lightScheme.harmonized(),
-    darkScheme.harmonized(),
-  );
-}
-
-List<Color> _extractAdditionalColours(ColorScheme scheme) => [
-      scheme.surface,
-      scheme.surfaceDim,
-      scheme.surfaceBright,
-      scheme.surfaceContainerLowest,
-      scheme.surfaceContainerLow,
-      scheme.surfaceContainer,
-      scheme.surfaceContainerHigh,
-      scheme.surfaceContainerHighest,
-    ];
-
-ColorScheme _insertAdditionalColours(
-  ColorScheme scheme,
-  List<Color> additionalColours,
-) =>
-    scheme.copyWith(
-      surface: additionalColours[0],
-      surfaceDim: additionalColours[1],
-      surfaceBright: additionalColours[2],
-      surfaceContainerLowest: additionalColours[3],
-      surfaceContainerLow: additionalColours[4],
-      surfaceContainer: additionalColours[5],
-      surfaceContainerHigh: additionalColours[6],
-      surfaceContainerHighest: additionalColours[7],
-    );
