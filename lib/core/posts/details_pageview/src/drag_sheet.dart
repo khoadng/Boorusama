db0d23786a4b083dfa2ad634e3af92c77848232e
khoa.nguyen <nguyenduckhoa1996@gmail.com>
2025-01-20 17:52:34 +0700

    feat: handle drag sheet when in full sheet state

--- lib/core/posts/details_pageview/src/drag_sheet.dart
+++ lib/core/posts/details_pageview/src/drag_sheet.dart
@@ -7,7 +7,9 @@ import 'post_details_page_view.dart';
 import 'post_details_page_view_controller.dart';
 import 'sheet_dragline.dart';
 
-const _kOverscrollThreshold = -6.0;
+const _kOverscrollSheetCloseThreshold = -6.0;
+const _kOverscrollFullSheetCloseThreshold = -24.0;
+const _kOverscrollFullSheetSnapbackThreshold = -6.0;
 const _kFullSheetSize = 0.9;
 const _kMinSheetSize = 0.0;
 const _kSnapAnimationDuration = Duration(milliseconds: 200);
@@ -38,24 +40,51 @@ class _DragSheetState extends State<DragSheet> {
     super.dispose();
   }
 
+  void _closeSheet() {
+    // Prevent multiple close
+    if (_closing) return;
+
+    _closing = true;
+    widget.pageViewController.resetSheet().then((value) => _closing = false);
+  }
+
+  double get maxSize => widget.pageViewController.maxSize;
+  bool get isFullyExpanded => sheetController.size == _kFullSheetSize;
+  DraggableScrollableController get sheetController =>
+      widget.pageViewController.sheetController;
+
+  void _snapSheetToMaxSize() {
+    sheetController.animateTo(
+      widget.pageViewController.maxSize,
+      duration: _kSnapAnimationDuration,
+      curve: Curves.easeInOut,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final maxSize = widget.pageViewController.maxSize;
     return NotificationListener<ScrollNotification>(
+      // Handles overscroll behavior based on the sheet's current state:
+      // - Fully Expanded: Closes the sheet if overscroll exceeds the close threshold or snaps back if within snapback threshold.
+      // - Partially Expanded: Closes the sheet if overscroll exceeds the close threshold.
       onNotification: (notification) {
-        // Auto close sheet when overscroll past a certain threshold
         if (notification is OverscrollNotification) {
           // Too fast, only close when sheet is still
           if (notification.velocity < 0) return false;
 
-          // Prevent multiple close
-          if (_closing) return false;
+          final overscrollAmount = notification.overscroll;
 
-          if (notification.overscroll < _kOverscrollThreshold) {
-            _closing = true;
-            widget.pageViewController
-                .resetSheet()
-                .then((value) => _closing = false);
+          if (isFullyExpanded) {
+            if (overscrollAmount < _kOverscrollFullSheetCloseThreshold) {
+              _closeSheet();
+            } else if (overscrollAmount <
+                _kOverscrollFullSheetSnapbackThreshold) {
+              _snapSheetToMaxSize();
+            }
+          } else {
+            if (overscrollAmount < _kOverscrollSheetCloseThreshold) {
+              _closeSheet();
+            }
           }
         }
 
@@ -65,7 +94,7 @@ class _DragSheetState extends State<DragSheet> {
         valueListenable: widget.pageViewController.sheetState,
         builder: (_, state, __) {
           return DraggableScrollableSheet(
-            controller: widget.pageViewController.sheetController,
+            controller: sheetController,
             // force maxSize when expanded
             initialChildSize: state.isExpanded ? maxSize : _kMinSheetSize,
             minChildSize: _kMinSheetSize,
