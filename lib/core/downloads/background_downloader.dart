6f798cf161bf6a784d5aada5362aa8811f7fd07d
khoa.nguyen <nguyenduckhoa1996@gmail.com>
2025-01-25 18:19:18 +0700

    fix: disable custom download location for Apple devices

--- lib/core/downloads/background_downloader.dart
+++ lib/core/downloads/background_downloader.dart
@@ -80,10 +80,16 @@ class BackgroundDownloader implements DownloadService {
             filename: filename,
             allowPause: true,
             retries: 1,
-            baseDirectory: downloadDir != null
-                ? BaseDirectory.root
-                : BaseDirectory.applicationDocuments,
-            directory: downloadDir != null ? downloadDir.path : '',
+            baseDirectory: isApple()
+                ? BaseDirectory.applicationDocuments
+                : downloadDir != null
+                    ? BaseDirectory.root
+                    : BaseDirectory.applicationDocuments,
+            directory: isApple()
+                ? 'Downloads'
+                : downloadDir != null
+                    ? downloadDir.path
+                    : '',
             updates: Updates.statusAndProgress,
             metaData: metadata?.toJsonString() ?? '',
             headers: headers,
@@ -111,8 +117,10 @@ class BackgroundDownloader implements DownloadService {
           final task = DownloadTask(
             url: url,
             filename: filename,
-            baseDirectory: BaseDirectory.root,
-            directory: path,
+            baseDirectory: isApple()
+                ? BaseDirectory.applicationDocuments
+                : BaseDirectory.root,
+            directory: isApple() ? 'Downloads' : path,
             allowPause: true,
             retries: 1,
             updates: Updates.statusAndProgress,
