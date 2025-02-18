bd598b204731b1a08987f075c4a73f675f5a713f
khoadng <nguyenduckhoa1996@gmail.com>
2025-02-18 18:27:35 +0700

    fix: update benefits

--- lib/core/premiums/src/types/premium.dart
+++ lib/core/premiums/src/types/premium.dart
@@ -3,27 +3,32 @@ import 'package:equatable/equatable.dart';
 
 const defaultBenefits = <Benefit>[
   Benefit(
-    title: 'Exclusive Themes',
+    title: 'Exclusive themes',
     description:
         'Enjoy a variety of themes to choose from and customize. Give each profile its own unique look.',
   ),
   Benefit(
-    title: 'Customize Your Layout',
+    title: 'Customize your layout',
     description:
         'Not a fan of the default layout? Want to see more or less info? Tweak it to your liking!',
   ),
   Benefit(
-    title: 'Custom Home Screen',
+    title: 'Custom home screen',
     description:
         'Set your home screen to Bookmarks, Search, or any other page you prefer for quick access.',
   ),
   Benefit(
-    title: 'Enhanced Bulk Downloader',
+    title: 'Enhanced bulk downloader',
     description:
         'Unlimited templates, multiple sessions and seamless resume after restart.',
   ),
   Benefit(
-    title: 'Support Development',
+    title: 'Early access',
+    description:
+        'Get exclusive early access to experimental features before they’re available to everyone else.',
+  ),
+  Benefit(
+    title: 'Support development',
     description:
         'Run by one developer since 2020, this project relies on your support for ongoing updates and improvements.',
   ),
