09362e9014df92d4ff98cc8adcb8a5603c9b7b66
khoadng <nguyenduckhoa1996@gmail.com>
2025-05-27 10:23:36 +0700

    Revert "feat: moebooru quick fav button (#514)" (#543)
    This reverts commit d0c349d3ea58c26faf903f70f621288a1879eab4.

--- lib/boorus/moebooru/feats/favorites/moebooru_favorites_notifier.dart
+++ lib/boorus/moebooru/feats/favorites/moebooru_favorites_notifier.dart
@@ -1,6 +1,4 @@
 // Package imports:
-import 'dart:math';
-
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -8,7 +6,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../../core/configs/ref.dart';
 import '../../moebooru.dart';
 
-var throttler = 0;
+CancelToken _cancelToken = CancelToken();
 
 class MoebooruFavoritesNotifier extends FamilyNotifier<Set<String>?, int> {
   @override
@@ -21,34 +19,28 @@ class MoebooruFavoritesNotifier extends FamilyNotifier<Set<String>?, int> {
     loadFavoriteUsers();
   }
 
-  void refresh() {
-    _loadFavoriteUsers();
-  }
+  Future<void> loadFavoriteUsers() async {
+    if (state != null) {
+      return;
+    }
 
-  Future<void> _loadFavoriteUsers() async {
-    throttler++;
-    await Future.delayed(
-        Duration(milliseconds: min(100 * (throttler - 1), 3000)));
-    throttler--;
+    // Cancel the previous request before making a new one
+    _cancelToken.cancel();
+
+    // Create a new CancelToken for the new request
+    _cancelToken = CancelToken();
 
     try {
       final client = ref.watch(moebooruClientProvider(ref.readConfigAuth));
 
       final users = await client.getFavoriteUsers(
         postId: arg,
-        cancelToken: CancelToken(),
+        cancelToken: _cancelToken,
       );
+
       state = users;
     } catch (e) {
       state = null;
     }
-  }
-
-  Future<void> loadFavoriteUsers() async {
-    if (state != null) {
-      return;
-    }
-
-    await _loadFavoriteUsers();
   }
 }
