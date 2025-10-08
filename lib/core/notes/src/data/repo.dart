a5a876dfd77ade2e87b14926f50b992c152c1077
khoa.nguyen <khoa.nguyen@fullertonhealth.com>
2025-10-08 10:09:53 +0700

    add create method

--- lib/core/notes/src/data/repo.dart
+++ lib/core/notes/src/data/repo.dart
@@ -6,15 +6,26 @@ class EmptyNoteRepository implements NoteRepository {
 
   @override
   Future<List<Note>> getNotes(int postId) async => [];
+
+  @override
+  Future<Note?> createNote(int postId, Note note) async => null;
 }
 
 class NoteRepositoryBuilder implements NoteRepository {
   const NoteRepositoryBuilder({
     required this.fetch,
+    this.create,
   });
 
   final Future<List<Note>> Function(int postId) fetch;
+  final Future<Note> Function(int postId, Note note)? create;
 
   @override
   Future<List<Note>> getNotes(int postId) => fetch(postId);
+
+  @override
+  Future<Note?> createNote(int postId, Note note) => switch (create) {
+    null => Future.sync(() => null),
+    final fn => fn(postId, note),
+  };
 }
