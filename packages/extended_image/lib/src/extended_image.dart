cdcedb27308acdb204d1a89903e4e638f89ca4fe
khoadng <nguyenduckhoa1996@gmail.com>
2025-11-26 22:10:21 +0700

    Revert "feat: stop reloading image when changing dependencies (experimental)"
    This reverts commit 88136c1a06d8539c66255b839a47a6664dafb7a3.

--- packages/extended_image/lib/src/extended_image.dart
+++ packages/extended_image/lib/src/extended_image.dart
@@ -409,7 +409,7 @@ class _ExtendedImageState extends State<ExtendedImage>
         avifFfi.disposeDecoder(key: oldWidget.image.hashCode.toString());
       }
 
-      _resolveImage(update: true);
+      _resolveImage();
     }
   }
 
@@ -447,12 +447,12 @@ class _ExtendedImageState extends State<ExtendedImage>
 
   @override
   void reassemble() {
-    _resolveImage(rebuild: true); // in case the image cache was flushed
+    _resolveImage(); // in case the image cache was flushed
     super.reassemble();
   }
 
   void reLoadImage() {
-    _resolveImage(rebuild: true);
+    _resolveImage(true);
   }
 
   Widget _getCompletedWidget() {
@@ -517,10 +517,7 @@ class _ExtendedImageState extends State<ExtendedImage>
     }
   }
 
-  void _resolveImage({
-    bool update = false,
-    bool rebuild = false,
-  }) {
+  void _resolveImage([bool rebuild = false]) {
     if (rebuild) {
       widget.image.evict();
     }
@@ -545,10 +542,7 @@ class _ExtendedImageState extends State<ExtendedImage>
       _controller.changeLoadState(LoadState.completed);
     }
 
-    _updateSourceStream(
-      newStream,
-      rebuild: rebuild,
-    );
+    _updateSourceStream(newStream, rebuild: rebuild);
   }
 
   /// Stops listening to the image stream, if this state object has attached a
@@ -580,11 +574,7 @@ class _ExtendedImageState extends State<ExtendedImage>
     }
   }
 
-  void _updateSourceStream(
-    ImageStream newStream, {
-    bool rebuild = false,
-    bool update = false,
-  }) {
+  void _updateSourceStream(ImageStream newStream, {bool rebuild = false}) {
     if (_imageStream?.key == newStream.key) {
       return;
     }
@@ -593,7 +583,7 @@ class _ExtendedImageState extends State<ExtendedImage>
       _imageStream?.removeListener(_getListener());
     }
 
-    if ((update && !widget.gaplessPlayback) || rebuild) {
+    if (!widget.gaplessPlayback || rebuild) {
       _controller.clearImage();
       _controller.changeLoadState(LoadState.loading);
     }
