5b110fc6b98ec17bad2c0d4c7ca0af135ebbb204
khoadng <nguyenduckhoa1996@gmail.com>
2026-03-14 22:34:46 +0700

    fix(zerochan): replace avif extension in sample and full image URLs

--- packages/booru_clients/lib/src/zerochan/types/post_dto.dart
+++ packages/booru_clients/lib/src/zerochan/types/post_dto.dart
@@ -41,10 +41,19 @@ extension PostDtoX on PostDto {
       .replaceAll('.240.', '.full.')
       .replaceAll('.600.', '.full.')
       .replaceAll('/240/', '/full/')
-      .replaceAll('/600/', '/full/');
+      .replaceAll('/600/', '/full/')
+      ._replaceAvifExtension();
 
   String? sampleUrl() => thumbnail
       ?.replaceAll(RegExp(r'/s\d+\.zerochan'), '/s3.zerochan')
       .replaceAll('.240.', '.600.')
-      .replaceAll('/240/', '/600/');
+      .replaceAll('/240/', '/600/')
+      ._replaceAvifExtension();
+}
+
+extension on String {
+  /// Zerochan only serves AVIF at thumbnail size (240px).
+  /// Larger sizes (600px, full) are served as JPG.
+  String _replaceAvifExtension() =>
+      endsWith('.avif') ? '${substring(0, length - 5)}.jpg' : this;
 }
